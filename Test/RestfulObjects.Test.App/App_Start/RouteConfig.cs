e1a87f2d750b6571495ac3e4dabe6b228176e434
scascarini <scascarini@nakedobjects.org>
2014-06-11 16:58:21 +0100

    #1050 continue updating packages

--- Test/RestfulObjects.Test.App/App_Start/RouteConfig.cs
+++ Test/RestfulObjects.Test.App/App_Start/RouteConfig.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+﻿using System.Web.Routing;
 
 namespace RestfulObjects.Test.App {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
