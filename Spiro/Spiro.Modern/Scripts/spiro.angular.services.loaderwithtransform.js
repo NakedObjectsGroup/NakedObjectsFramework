0c6322966433daaf39375243c1f1b0b8d948d7e9
scascarini <scascarini@nakedobjects.org>
2014-07-07 15:20:26 +0100

    add transform based services

---
+++ Spiro/Spiro.Modern/Scripts/spiro.angular.services.loaderwithtransform.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="typings/angularjs/angular.d.ts" />
+/// <reference path="spiro.models.ts" />
+var Spiro;
+(function (Spiro) {
+    (function (Angular) {
+        Angular.app.service('loaderWithTransform', function (repLoader, $q, transformStrategy) {
+            var transformer = this;
+
+            transformer.loadAndTransform = function tt(url, repType) {
+                var obj = new repType({});
+                obj.hateoasUrl = url;
+                return repLoader.populate(obj).then(function (resource) {
+                    return transformStrategy.transform(resource, transformer);
+                });
+            };
+        });
+    })(Spiro.Angular || (Spiro.Angular = {}));
+    var Angular = Spiro.Angular;
+})(Spiro || (Spiro = {}));
+//# sourceMappingURL=spiro.angular.services.loaderwithtransform.js.map
