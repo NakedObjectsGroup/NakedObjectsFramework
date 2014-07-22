628525fd6b5f42fe853d4240f1d250b0dc8c81da
scascarini <scascarini@nakedobjects.org>
2014-07-22 15:48:46 +0100

    Deleting Spiro code out of framework
    Code has been moved into its own repositories

--- Spiro/Spiro.Modern/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="jquery-ui-1.10.4.js" />
-/// <reference path="jquery-2.1.1.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
-/// <reference path="modernizr-2.6.2.js" />
--- Spiro/Spiro.Modern/SpiroReadMe.txt
+++
@@ -1,8 +0,0 @@
-﻿To start update spiro.config.ts/js with the path to your Restful Objects server (appPath). 
-Remember CORS. 
-
-Other customisations are in spiro.angular.config.ts/js. 
-
-
-
- 
