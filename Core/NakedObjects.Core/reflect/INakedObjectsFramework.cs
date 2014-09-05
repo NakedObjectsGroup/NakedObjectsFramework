cf9cdea74939909cbaabbc18ff8331d02c379655
Richard Pawson <rpawson@nakedobjects.net>
2014-09-05 16:40:31 +0100

    Comment only

--- Core/NakedObjects.Core/reflect/INakedObjectsFramework.cs
+++ Core/NakedObjects.Core/reflect/INakedObjectsFramework.cs
@@ -6,6 +6,11 @@ using NakedObjects.Core.Reflect;
 using NakedObjects.Objects;
 
 namespace NakedObjects {
+    /// <summary>
+    /// Defines a service that provides easy access to the principal components of the framework.
+    /// An implementation of this service interface will be injected into any domain
+    /// object that needs it.
+    /// </summary>
     public interface INakedObjectsFramework {
         IMessageBroker MessageBroker { get; }
 
@@ -18,6 +23,7 @@ namespace NakedObjects {
         INakedObjectReflector Reflector { get; }
 
         IAuthorizationManager AuthorizationManager { get; }
+
         IContainerInjector Injector { get; }
     }
 }
