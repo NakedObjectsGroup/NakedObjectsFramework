4b1d5d2dbee5de1dbef64eea0c95f08cc5aa23bd
scascarini <scascarini@nakedobjects.org>
2014-09-17 08:41:27 +0100

    #4137 Temp remove RO end to end test

--- Test/RestfulObjects.Test.App/App_Start/UnityWebApiActivator.cs
+++ Test/RestfulObjects.Test.App/App_Start/UnityWebApiActivator.cs
@@ -5,6 +5,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+using System.Web;
 using System.Web.Http;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.WebApi;
