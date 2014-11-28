75a679625eaae0245ca379e0442d255b5be6c995
Richard Pawson <rpawson@nakedobjects.net>
2014-11-28 16:25:49 +0000

    IMenu, ITypedMenu added to NakedObjects.Types in PM

---
+++ Programming Model/NakedObjects.Types/Menu/IMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+namespace NakedObjects.Menu {
+
+    /// <summary>
+    /// Extension of IMenuImmutable that provides methods for building the menu
+    /// during reflection time.
+    /// </summary>
+    public interface IMenu  {
+
+        /// <summary>
+        /// Adds specified action as the next menu item
+        /// </summary>
+        /// <typeparam name="TObject"></typeparam>
+        /// <param name="actionName"></param>
+        /// <param name="renamedTo"></param>
+        /// <returns>This menu (for fluent programming)</returns>
+        IMenu AddActionFrom<TObject>(string actionName, string renamedTo = null);
+
+        /// <summary>
+        /// Adds all actions from the service not previously added individually,
+        /// in the order they are specified in the service.
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <returns>This menu (for fluent programming)</returns>
+        IMenu AddAllRemainingActionsFrom<TService>();
+
+        /// <summary>
+        /// </summary>
+        /// <param name="subMenuName"></param>
+        /// <returns>The new menu, which will already have been added to the hosting menu</returns>
+        IMenu CreateSubMenu(string subMenuName);
+    }
+}
