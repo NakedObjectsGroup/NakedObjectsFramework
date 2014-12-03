ea424c64175968474e0cf2308b00b8be4b31e334
scascarini <scascarini@nakedobjects.org>
2014-12-03 12:03:23 +0000

    #4137 Fix test

--- Core/NakedObjects.Architecture/SpecImmutable/ActionsForService.cs
+++ Core/NakedObjects.Architecture/SpecImmutable/ActionsForService.cs
@@ -5,9 +5,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+using System;
 using System.Collections.Generic;
 
 namespace NakedObjects.Architecture.SpecImmutable {
+    [Serializable]
     public class ActionsForService {
         public string Id { get; set; }
         public string Name { get; set; }
