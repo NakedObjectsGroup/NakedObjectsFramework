d8ed074e6750ab193adbfeda25dfc8ce35ad731c
Richard Pawson <rpawson@nakedobjects.net>
2015-02-02 12:17:32 +0000

    Version 6.0.3 -  Moved up to .NET 4.5.2, Owin 3.0 & latest JQuery dependencies

--- MVC/NakedObjects.Mvc.App/Scripts/_references.js
+++ MVC/NakedObjects.Mvc.App/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-2.1.0.js" />
-/// <reference path="jquery-ui-1.10.4.js" />
+﻿/// <reference path="jquery-ui-1.11.2.js" />
+/// <reference path="jquery-2.1.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="bootstrap.js" />
