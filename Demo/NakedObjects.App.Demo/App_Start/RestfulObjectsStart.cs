418b1145c95182d9079f1f31d33cee2872741459
scascarini <scascarini@nakedobjects.org>
2015-02-05 10:12:08 +0000

    version 6.0.3 more package updates

---
+++ Demo/NakedObjects.App.Demo/App_Start/RestfulObjectsStart.cs
@@ -0,0 +1,24 @@
+// Copyright � Naked Objects Group Ltd ( http://www.nakedobjects.net). 
+// All Rights Reserved. This code released under the terms of the 
+// Microsoft Public License (MS-PL) ( http://opensource.org/licenses/ms-pl.html) 
+
+using System.Web.Routing;
+using NakedObjects.App.Demo;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof (RestfulObjectsStart), "PreStart")]
+[assembly: PostApplicationStartMethod(typeof (RestfulObjectsStart), "PostStart")]
+
+namespace NakedObjects.App.Demo {
+    public static class RestfulObjectsStart {
+        public static void PreStart() {
+            RestConfig.RestPreStart();
+            RestConfig.RegisterRestfulObjectsRoutes(RouteTable.Routes);
+        }
+
+        public static void PostStart() {
+            RunWeb.Run();
+            RestConfig.RestPostStart();
+        }
+    }
+}
