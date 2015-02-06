9483d0bd10fd3ad0e1535c38189a2bef98fce000
Richard Pawson <rpawson@nakedobjects.net>
2015-02-06 08:55:34 +0000

    Fixed some F# compile errors - some remaining?

--- Core/NakedObjects.Persistor.Entity.Test/EntityTestSuite.fs
+++ Core/NakedObjects.Persistor.Entity.Test/EntityTestSuite.fs
@@ -139,11 +139,11 @@ type EntityTestSuite() =
         config.UsingCodeFirstContext(Func<Data.Entity.DbContext>(f)) |> ignore
         container.RegisterInstance(typeof<IEntityObjectStoreConfiguration>, null, config, (new ContainerControlledLifetimeManager())) |> ignore
         let types = [| typeof<TestData.Person>;typeof<TestData.Order>;typeof<TestData.OrderFail>;typeof<TestData.Person[]>;typeof<EntityCollection<TestData.Person>>;typeof<System.Collections.Generic.List<TestData.Person>>  |]
-        let ms = [| typeof<SimpleRepository<Person>> |]
-        let ca = [| typeof<SimpleRepository<Product>> |]
-        let ss = [| typeof<SimpleRepository<Address>> |]
-        let ns = [| "TestData"   |]
-        let reflectorConfig = new ReflectorConfiguration(types, ms, ca, ss, ns)
+        let services = [| typeof<SimpleRepository<Person>>; 
+                        typeof<SimpleRepository<Product>>;
+                         typeof<SimpleRepository<Address>> |]
+        let namespaces = [| "TestData"   |]
+        let reflectorConfig = new ReflectorConfiguration(types, services, namespaces)
         container.RegisterInstance(typeof<IReflectorConfiguration>, null, reflectorConfig, (new ContainerControlledLifetimeManager())) |> ignore
         ()
     
