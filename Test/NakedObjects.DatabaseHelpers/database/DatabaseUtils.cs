09a6574ee90ca75bddbe14012fe67448c3bd0bf3
scascarini <scascarini@nakedobjects.org>
2015-02-20 14:37:28 +0000

    #4151 fix licence and formatting

--- Test/NakedObjects.DatabaseHelpers/database/DatabaseUtils.cs
+++ Test/NakedObjects.DatabaseHelpers/database/DatabaseUtils.cs
@@ -1,6 +1,10 @@
-// Copyright © Naked Objects Group Ltd ( http://www.nakedobjects.net). 
-// All Rights Reserved. This code released under the terms of the 
-// Microsoft Public License (MS-PL) ( http://opensource.org/licenses/ms-pl.html) 
+// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 using System;
 using System.Data.SqlClient;
 using System.IO;
@@ -17,14 +21,11 @@ namespace NakedObjects.DatabaseHelpers {
     /// DatabaseUtils.RevertDatabaseFromSnapshot("AdventureWorks", "startstate");
     /// </summary>
     public static class DatabaseUtils {
-
 #if AV 
         private const string Server = @"(local)\SQL2012SP1";
 #else
         private const string Server = @".\SQLEXPRESS";
 #endif
-
-
 
         public static void BackupDatabase(string databaseName, string backupName, string serverName = Server) {
             Server server = string.IsNullOrEmpty(serverName) ? new Server() : new Server(serverName);
@@ -80,10 +81,10 @@ namespace NakedObjects.DatabaseHelpers {
                     string logicalName = new Server().Databases[databaseName].FileGroups[0].Files[0].Name;
 
                     command.CommandText = string.Format("CREATE DATABASE {0} ON (NAME = {1}, FILENAME = '{2}') AS SNAPSHOT OF {3};",
-                                                        Path.GetFileNameWithoutExtension(snapshotName),
-                                                        logicalName,
-                                                        snapshotFile,
-                                                        databaseName);
+                        Path.GetFileNameWithoutExtension(snapshotName),
+                        logicalName,
+                        snapshotFile,
+                        databaseName);
                     connection.Open();
                     command.ExecuteNonQuery();
                 }
@@ -99,8 +100,8 @@ namespace NakedObjects.DatabaseHelpers {
                     command.Connection = connection;
                     command.CommandTimeout = 1000;
                     command.CommandText = string.Format("RESTORE DATABASE {0} FROM DATABASE_SNAPSHOT = '{1}'; DROP DATABASE {1};",
-                                                        databaseName,
-                                                        snapshotName);
+                        databaseName,
+                        snapshotName);
                     connection.Open();
                     command.ExecuteNonQuery();
                 }
