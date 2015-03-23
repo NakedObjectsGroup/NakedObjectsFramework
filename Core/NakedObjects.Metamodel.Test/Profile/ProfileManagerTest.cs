26fdac32141371e4c2bc771e5ed2a082559ea283
scascarini <scascarini@nakedobjects.org>
2015-03-23 09:36:50 +0000

    #11237 Profiler tests

--- Core/NakedObjects.Metamodel.Test/Profile/ProfileManagerTest.cs
+++ Core/NakedObjects.Metamodel.Test/Profile/ProfileManagerTest.cs
@@ -25,6 +25,60 @@ namespace NakedObjects.Meta.Test.Profile {
 
         #endregion
 
+        private static Mock<ISpecification> SetupMocks(out Mock<ISpecification> testHolder) {
+            var testSpec = new Mock<ISpecification>();
+            testHolder = new Mock<ISpecification>();
+            var identifier = new Mock<IIdentifier>();
+
+            testHolder.Setup(h => h.Identifier).Returns(identifier.Object);
+            testSpec.Setup(s => s.Identifier).Returns(identifier.Object);
+            return testSpec;
+        }
+
+        private static void TestDecorated<TFacet, TResult>(ProfileManager manager) where TFacet : class, IFacet {
+            Mock<ISpecification> testHolder;
+            var testSpec = SetupMocks(out testHolder);
+            var testFacet = new Mock<TFacet>();
+
+            testFacet.Setup(n => n.FacetType).Returns(typeof (TFacet));
+            testFacet.Setup(n => n.Specification).Returns(testSpec.Object);
+
+            var facet = manager.Decorate(testFacet.Object, testHolder.Object);
+
+            Assert.IsInstanceOfType(facet, typeof (TResult));
+        }
+
+        private static void TestNotDecorated<TFacet, TResult>(ProfileManager manager) where TFacet : class, IFacet {
+            Mock<ISpecification> testHolder;
+            var testSpec = SetupMocks(out testHolder);
+            var testFacet = new Mock<TFacet>();
+
+            testFacet.Setup(n => n.FacetType).Returns(typeof(TFacet));
+            testFacet.Setup(n => n.Specification).Returns(testSpec.Object);
+
+            var facet = manager.Decorate(testFacet.Object, testHolder.Object);
+
+            Assert.IsNotInstanceOfType(facet, typeof(TResult));
+            Assert.IsInstanceOfType(facet, typeof(TFacet));
+
+        }
+
+        private static void TestDecorateFacet<TFacet, TResult>(ProfileEvent eventToTest) where TFacet : class, IFacet {
+            var config = new Mock<IProfileConfiguration>();
+            var auditor = new Mock<IProfiler>();
+
+            config.Setup(c => c.Profiler).Returns(auditor.Object.GetType());
+            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent> {eventToTest});
+
+            var manager = new ProfileManager(config.Object);
+
+            TestDecorated<TFacet, TResult>(manager);
+        }
+
+        private void TestDecorateCallbackFacet<T>(ProfileEvent eventToTest) where T : class, ICallbackFacet {
+            TestDecorateFacet<T, ProfileCallbackFacet>(eventToTest);
+        }
+
         [TestMethod]
         public void TestCreateOk() {
             var config = new ProfileConfiguration<IProfiler>();
@@ -52,91 +106,115 @@ namespace NakedObjects.Meta.Test.Profile {
 
         [TestMethod]
         public void TestDecorateActionInvocationFacet() {
-            var config = new Mock<IProfileConfiguration>();
-            var auditor = new Mock<IProfiler>();
-
-            config.Setup(c => c.Profiler).Returns(auditor.Object.GetType());
-
-            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent>() {ProfileEvent.ActionInvocation});
-
-            var manager = new ProfileManager(config.Object);
-
-            var testSpec = new Mock<ISpecification>();
-            var testHolder = new Mock<ISpecification>();
-            var identifier = new Mock<IIdentifier>();
-            var testFacet = new Mock<IActionInvocationFacet>();
-
-            testHolder.Setup(h => h.Identifier).Returns(identifier.Object);
-
-            testSpec.Setup(s => s.Identifier).Returns(identifier.Object);
-
-            testFacet.Setup(n => n.FacetType).Returns(typeof (IActionInvocationFacet));
-
-            testFacet.Setup(n => n.Specification).Returns(testSpec.Object);
-
-            var facet = manager.Decorate(testFacet.Object, testHolder.Object);
-
-            Assert.IsInstanceOfType(facet, typeof (ProfileActionInvocationFacet));
+            TestDecorateFacet<IActionInvocationFacet, ProfileActionInvocationFacet>(ProfileEvent.ActionInvocation);
         }
 
         [TestMethod]
-        public void TestDecorateUpdatedFacet() {
-            var config = new Mock<IProfileConfiguration>();
-            var auditor = new Mock<IProfiler>();
+        public void TestDecoratePropertySetFacet() {
+            TestDecorateFacet<IPropertySetterFacet, ProfilePropertySetterFacet>(ProfileEvent.PropertySet);
+        }
 
-            config.Setup(c => c.Profiler).Returns(auditor.Object.GetType());
+        [TestMethod]
+        public void TestDecorateCreatedFacet() {
+            TestDecorateCallbackFacet<ICreatedCallbackFacet>(ProfileEvent.Created);
+        }
 
-            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent>() { ProfileEvent.Updated });
+        [TestMethod]
+        public void TestDecorateDeletedFacet() {
+            TestDecorateCallbackFacet<IDeletedCallbackFacet>(ProfileEvent.Deleted);
+        }
 
+        [TestMethod]
+        public void TestDecorateDeletingFacet() {
+            TestDecorateCallbackFacet<IDeletingCallbackFacet>(ProfileEvent.Deleting);
+        }
 
-            var manager = new ProfileManager(config.Object);
+        [TestMethod]
+        public void TestDecorateLoadedFacet() {
+            TestDecorateCallbackFacet<ILoadedCallbackFacet>(ProfileEvent.Loaded);
+        }
 
-            var testSpec = new Mock<ISpecification>();
-            var testHolder = new Mock<ISpecification>();
-            var identifier = new Mock<IIdentifier>();
-            var testFacet = new Mock<IUpdatedCallbackFacet>();
-
-            testHolder.Setup(h => h.Identifier).Returns(identifier.Object);
-
-            testSpec.Setup(s => s.Identifier).Returns(identifier.Object);
-
-            testFacet.Setup(n => n.FacetType).Returns(typeof (IUpdatedCallbackFacet));
-
-            testFacet.Setup(n => n.Specification).Returns(testSpec.Object);
-
-            var facet = manager.Decorate(testFacet.Object, testHolder.Object);
-
-            Assert.IsInstanceOfType(facet, typeof (ProfileCallbackFacet));
+        [TestMethod]
+        public void TestDecorateLoadingFacet() {
+            TestDecorateCallbackFacet<ILoadingCallbackFacet>(ProfileEvent.Loading);
         }
 
         [TestMethod]
         public void TestDecoratePersistedFacet() {
+            TestDecorateCallbackFacet<IPersistedCallbackFacet>(ProfileEvent.Persisted);
+        }
+
+        [TestMethod]
+        public void TestDecoratePersistingFacet() {
+            TestDecorateCallbackFacet<IPersistingCallbackFacet>(ProfileEvent.Persisting);
+        }
+
+        [TestMethod]
+        public void TestDecorateUpdatedFacet() {
+            TestDecorateCallbackFacet<IUpdatedCallbackFacet>(ProfileEvent.Updated);
+        }
+
+        [TestMethod]
+        public void TestDecorateUpdatingFacet() {
+            TestDecorateCallbackFacet<IUpdatingCallbackFacet>(ProfileEvent.Updating);
+        }
+
+        [TestMethod]
+        public void TestDecorateAllFacets() {
             var config = new Mock<IProfileConfiguration>();
             var auditor = new Mock<IProfiler>();
 
             config.Setup(c => c.Profiler).Returns(auditor.Object.GetType());
-
-            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent>() { ProfileEvent.Persisted });
-
+            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent> {
+                ProfileEvent.ActionInvocation,
+                ProfileEvent.PropertySet,
+                ProfileEvent.Created,
+                ProfileEvent.Deleted,
+                ProfileEvent.Deleting,
+                ProfileEvent.Loaded,
+                ProfileEvent.Loading,
+                ProfileEvent.Persisted,
+                ProfileEvent.Persisting,
+                ProfileEvent.Updated,
+                ProfileEvent.Updating
+            });
 
             var manager = new ProfileManager(config.Object);
 
-            var testSpec = new Mock<ISpecification>();
-            var testHolder = new Mock<ISpecification>();
-            var identifier = new Mock<IIdentifier>();
-            var testFacet = new Mock<IPersistedCallbackFacet>();
+            TestDecorated<IActionInvocationFacet, ProfileActionInvocationFacet>(manager);
+            TestDecorated<IPropertySetterFacet, ProfilePropertySetterFacet>(manager);
+            TestDecorated<ICreatedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IDeletedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IDeletingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<ILoadedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<ILoadingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IPersistedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IPersistingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IUpdatedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestDecorated<IUpdatingCallbackFacet, ProfileCallbackFacet>(manager);
+        }
 
-            testHolder.Setup(h => h.Identifier).Returns(identifier.Object);
+        [TestMethod]
+        public void TestDecorateNoFacets() {
+            var config = new Mock<IProfileConfiguration>();
+            var auditor = new Mock<IProfiler>();
 
-            testSpec.Setup(s => s.Identifier).Returns(identifier.Object);
+            config.Setup(c => c.Profiler).Returns(auditor.Object.GetType());
+            config.Setup(c => c.EventsToProfile).Returns(new HashSet<ProfileEvent> {});
 
-            testFacet.Setup(n => n.FacetType).Returns(typeof (IPersistedCallbackFacet));
+            var manager = new ProfileManager(config.Object);
 
-            testFacet.Setup(n => n.Specification).Returns(testSpec.Object);
-
-            var facet = manager.Decorate(testFacet.Object, testHolder.Object);
-
-            Assert.IsInstanceOfType(facet, typeof (ProfileCallbackFacet));
+            TestNotDecorated<IActionInvocationFacet, ProfileActionInvocationFacet>(manager);
+            TestNotDecorated<IPropertySetterFacet, ProfilePropertySetterFacet>(manager);
+            TestNotDecorated<ICreatedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IDeletedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IDeletingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<ILoadedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<ILoadingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IPersistedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IPersistingCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IUpdatedCallbackFacet, ProfileCallbackFacet>(manager);
+            TestNotDecorated<IUpdatingCallbackFacet, ProfileCallbackFacet>(manager);
         }
     }
 }
