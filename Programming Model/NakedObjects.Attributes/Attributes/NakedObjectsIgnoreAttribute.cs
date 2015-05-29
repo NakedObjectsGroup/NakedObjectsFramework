b89ce994c1fd0dd2513d763bafada81f9283d3a1
Richard Pawson <rpawson@nakedobjects.net>
2015-05-29 17:10:01 +0100

    NakedObjectsIgnore dis-abled again for class-level use (in anticipation of #13251 in 7.1)

--- Programming Model/NakedObjects.Attributes/Attributes/NakedObjectsIgnoreAttribute.cs
+++ Programming Model/NakedObjects.Attributes/Attributes/NakedObjectsIgnoreAttribute.cs
@@ -11,6 +11,6 @@ namespace NakedObjects {
     /// <summary>
     ///     Tell Nakedobjects to ignore this class, property or action. It will not be introspected.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class| AttributeTargets.Method | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public class NakedObjectsIgnoreAttribute : Attribute {}
 }
