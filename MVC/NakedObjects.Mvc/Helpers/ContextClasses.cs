9a78ed4552611224d17cc4f63340f4596308843f
scascarini <scascarini@nakedobjects.org>
2015-06-17 16:07:34 +0100

    code tidy

--- MVC/NakedObjects.Mvc/Helpers/ContextClasses.cs
+++ MVC/NakedObjects.Mvc/Helpers/ContextClasses.cs
@@ -30,7 +30,6 @@ namespace NakedObjects.Web.Mvc.Html {
     internal abstract class FeatureContext : ObjectContext {
         protected FeatureContext(IIdHelper idHelper, ObjectContext otherContext) : base(idHelper, otherContext) {}
         protected FeatureContext(IIdHelper idHelper, IObjectFacade target) : base(idHelper, target) {}
-        public abstract IFacadeHolder Feature { get; }
     }
 
     internal class PropertyContext : FeatureContext {
@@ -53,10 +52,6 @@ namespace NakedObjects.Web.Mvc.Html {
 
         public PropertyContext ParentContext { get; set; }
         public IAssociationFacade Property { get; set; }
-
-        public override IFacadeHolder Feature {
-            get { return Property; }
-        }
 
         public bool IsEdit { get; private set; }
         public bool IsPropertyEdit { get; set; }
@@ -139,10 +134,6 @@ namespace NakedObjects.Web.Mvc.Html {
         public IActionFacade Action { get; set; }
         public RouteValueDictionary ParameterValues { get; set; }
 
-        public override IFacadeHolder Feature {
-            get { return Action; }
-        }
-
         public ParameterContext[] GetParameterContexts(IFrameworkFacade facade) {
             if (parameterContexts == null) {
                 parameterContexts = Action.Parameters.Where(Filter).Select(p => new ParameterContext(IdHelper, EmbeddedInObject, Target, Action, p, true)).ToArray();
@@ -209,10 +200,6 @@ namespace NakedObjects.Web.Mvc.Html {
         public bool IsHidden { get; set; }
         public IActionParameterFacade Parameter { get; set; }
         public IObjectFacade CustomValue { get; set; }
-
-        public override IFacadeHolder Feature {
-            get { return Parameter; }
-        }
 
         public bool IsParameterEdit { get; set; }
 
