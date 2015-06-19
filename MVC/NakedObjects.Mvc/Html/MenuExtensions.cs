411537909ce604fe373e1017ed645fa08b1e98b3
Richard Pawson <rpawson@nakedobjects.net>
2015-06-19 11:55:45 +0100

    Exposed new helper method to facilitate custom menus

--- MVC/NakedObjects.Mvc/Html/MenuExtensions.cs
+++ MVC/NakedObjects.Mvc/Html/MenuExtensions.cs
@@ -73,10 +73,14 @@ namespace NakedObjects.Web.Mvc.Html {
         private static MvcHtmlString RenderMainMenus(this HtmlHelper html, IEnumerable<IMenuImmutable> menus) {
             var tag = new TagBuilder("div");
             tag.AddCssClass(IdHelper.ServicesContainerName);
+            AddMainMenusIntoTag(html, menus, tag);
+            return MvcHtmlString.Create(tag.ToString());
+        }
+
+        public static void AddMainMenusIntoTag(this HtmlHelper html, IEnumerable<IMenuImmutable> menus, TagBuilder tag) {
             foreach (IMenuImmutable menu in menus) {
                 tag.InnerHtml += html.MenuAsHtml(menu, null, false, false);
             }
-            return MvcHtmlString.Create(tag.ToString());
         }
 
         private static MvcHtmlString MenuAsHtml(this HtmlHelper html, IMenuImmutable menu, INakedObjectAdapter nakedObject, bool isEdit, bool defaultToEmptyMenu) {
