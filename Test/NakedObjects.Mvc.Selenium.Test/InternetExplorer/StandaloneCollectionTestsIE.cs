c77f546aa45bbe13d55e45cc9659c18ae14a591c
scascarini <scascarini@nakedobjects.org>
2015-07-20 15:34:53 +0100

    #13628 add tests

--- Test/NakedObjects.Mvc.Selenium.Test/InternetExplorer/StandaloneCollectionTestsIE.cs
+++ Test/NakedObjects.Mvc.Selenium.Test/InternetExplorer/StandaloneCollectionTestsIE.cs
@@ -89,5 +89,10 @@ namespace NakedObjects.Mvc.Selenium.Test.InternetExplorer {
         public override void PagingWithFormat() {
             DoPagingWithFormat();
         }
+
+        [TestMethod]
+        public override void InvokeActionNoResultAfterCollection() {
+            DoInvokeActionNoResultAfterCollection();
+        }
     }
 }
