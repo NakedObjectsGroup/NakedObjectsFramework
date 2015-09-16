0a2f05af437004f01d7cff7adc486de3e8c5e685
scascarini <scascarini@nakedobjects.org>
2015-09-16 14:52:08 +0100

    Starting to add some async testing

---
+++ Test/NakedObjects.Batch.Test/BatchStartPoint.cs
@@ -0,0 +1,44 @@
+// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventureWorksModel;
+using NakedObjects.Architecture.Component;
+using NakedObjects.Async;
+
+namespace NakedObjects.Batch {
+    public class BatchStartPoint : IBatchStartPoint {
+        public IAsyncService AsyncService { private get; set; }
+
+        #region IBatchStartPoint Members
+
+        public void Execute() {
+            Console.WriteLine("Starting");
+            var tasks = new Task[4];
+            tasks[0] = AsyncService.RunAsync((doc) => { DoWork(doc, 63290); });
+            tasks[1] = AsyncService.RunAsync((doc) => { DoWork(doc, 63291); });
+            tasks[2] = AsyncService.RunAsync((doc) => { DoWork(doc, 63292); });
+            tasks[3] = AsyncService.RunAsync((doc) => { DoWork(doc, 63293); });
+            Console.WriteLine("Waiting");
+            Task.WaitAll(tasks);
+            Console.WriteLine("All done");
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        private void DoWork(IDomainObjectContainer container, int orderId) {
+            var order = container.Instances<SalesOrderHeader>().FirstOrDefault(soh => soh.SalesOrderID == orderId);
+            if (order != null) {
+                order.AddComment("Adding comment by Async");
+                Console.WriteLine("Added comment to Order " + orderId);
+            }
+        }
+    }
+}
---
+++ Test/NakedObjects.Batch.Test/BatchTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NakedObjects.Architecture.Component;
+
+namespace NakedObjects.Batch {
+    [TestClass]
+    public class BatchTest {
+
+        [TestInitialize]
+        public void Start() {
+            UnityActivator.Start();
+        }
+
+        [TestCleanup]
+        public void End() {
+            UnityActivator.Shutdown();
+        }
+
+        [TestMethod]
+        public void TestBatch() {
+            UnityConfig.GetConfiguredContainer().Resolve<IBatchRunner>().Run(new BatchStartPoint());
+        }
+    }
+}
