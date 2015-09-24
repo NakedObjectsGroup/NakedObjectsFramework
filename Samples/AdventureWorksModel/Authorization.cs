1baad09a6f4ad80c6562ee4e7f623255a3d9bf34
Richard Pawson <rpawson@nakedobjects.net>
2015-09-24 12:47:14 +0100

    Added authorizers to AW model

---
+++ Samples/AdventureWorksModel/Authorization.cs
@@ -0,0 +1,31 @@
+﻿using NakedObjects.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksModel {
+    public class DefaultAuthorizer : ITypeAuthorizer<object> {
+
+        public bool IsEditable(IPrincipal principal, object target, string memberName) {
+            return true;
+        }
+
+        public bool IsVisible(IPrincipal principal, object target, string memberName) {
+            return true;
+        }
+    }
+
+    //Used to test that a specific Finder Action can be hidden by authorization.
+    public class EmployeeRepositoryAuthorizer : ITypeAuthorizer<EmployeeRepository> {
+        public bool IsEditable(IPrincipal principal, EmployeeRepository target, string memberName) {
+            return true;
+        }
+        public bool IsVisible(IPrincipal principal, EmployeeRepository target, string memberName) {
+            if (memberName == "FindRecentHires") return false;
+            return true;
+        }
+    }
+}
--- Test/NakedObjects.Test.App/App_Start/NakedObjectsRunSettings.cs
+++ Test/NakedObjects.Test.App/App_Start/NakedObjectsRunSettings.cs
@@ -96,28 +96,6 @@ namespace NakedObjects.Test.App {
             }
         }
 
-        public class DefaultAuthorizer : ITypeAuthorizer<object> {
-
-            public bool IsEditable(IPrincipal principal, object target, string memberName) {
-                return true;
-            }
-
-            public bool IsVisible(IPrincipal principal, object target, string memberName) {
-                return true;
-            }
-        }
-
-        //Used to test that a specific Finder Action can be hidden by authorization.
-        public class EmployeeRepositoryAuthorizer : ITypeAuthorizer<EmployeeRepository> {
-            public bool IsEditable(IPrincipal principal, EmployeeRepository target, string memberName) {
-                return true;
-            }
-            public bool IsVisible(IPrincipal principal, EmployeeRepository target, string memberName) {
-                if (memberName == "FindRecentHires") return false;
-                return true;
-            }
-        }
-
         public static IAuditConfiguration AuditConfig() {
             return new AuditConfiguration<DefaultAuditor>();
         }
