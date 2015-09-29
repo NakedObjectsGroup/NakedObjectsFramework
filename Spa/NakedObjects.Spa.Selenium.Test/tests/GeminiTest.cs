23317efc01a13e1662f97347ca3c9de98512e12f
scascarini <scascarini@nakedobjects.org>
2015-09-29 16:07:44 +0100

    more selenium tests

--- Spa/NakedObjects.Spa.Selenium.Test/tests/GeminiTest.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/GeminiTest.cs
@@ -296,6 +296,9 @@ namespace NakedObjects.Web.UnitTests.Selenium {
         #region Editing & Saving
         protected void EditObject()
         {
+            // wait unitil object is actually displayed 
+            wait.Until(d => d.FindElements(By.CssSelector(".property")).Count > 0);
+
             Click(GetEditButton());
             GetSaveButton();
             GetCancelEditButton();
