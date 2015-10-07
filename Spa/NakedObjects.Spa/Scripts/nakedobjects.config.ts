7bbc1a971b70964e3c609ebd8a69e22564ebf4cc
scascarini <scascarini@nakedobjects.org>
2015-10-07 14:51:19 +0100

    Update spa to point at azure rest demo

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.config.ts
@@ -15,7 +15,7 @@ module NakedObjects {
     // custom configuration for a particular implementation 
 
     // path to Restful Objects server 
-    export var appPath = "http://mvc.nakedobjects.net:1081/RestDemo";
+    export var appPath = "http://nakedobjectsrodemo.azurewebsites.net";
     // export var appPath = "http://localhost:61546";
 
     // path local server (ie ~) only needed if you want to hard code paths for some reason
