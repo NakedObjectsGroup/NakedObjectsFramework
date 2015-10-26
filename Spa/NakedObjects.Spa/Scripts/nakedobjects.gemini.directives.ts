0abbc975644ce298d9abf063b1540f8d429688e5
Richard Pawson <rpawson@nakedobjects.net>
2015-10-26 12:41:51 +0000

    Styling for clipboard

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.gemini.directives.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.gemini.directives.ts
@@ -334,11 +334,10 @@ module NakedObjects.Angular.Gemini {
                 const compiledClone = $compile(`<div class='reference ${draggableVm.color}' gemini-drag=''>${element[0].innerText}</div>`)(scope);        
                 compiledClone.data(draggableVmKey, draggableVm);
                 $("div.footer div.currentcopy").empty();
-                $("div.footer div.currentcopy").append(compiledClone);
+                $("div.footer div.currentcopy").append("<span>Clipboard:</span>").append(compiledClone);
                 event.preventDefault();
             }
         });
-
     });
 
     app.directive("geminiEnter", () => (scope, element, attrs) => {
