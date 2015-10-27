32444172d589b8c07b89b422ee22efec76f38d9d
Richard Pawson <rpawson@nakedobjects.net>
2015-10-27 11:25:20 +0000

    Tests for copying reference into 'clipboard'

--- Spa/NakedObjects.Spa.Selenium.Test/tests/CopyAndPasteTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/CopyAndPasteTests.cs
@@ -14,23 +14,74 @@ namespace NakedObjects.Web.UnitTests.Selenium {
 
     public abstract class CopyAndPasteTests : AWTest {
 
-        [TestMethod, Ignore]
-        public virtual void CopiedObjectShowsInFooter()
+        [TestMethod]
+        public virtual void CopyTitleOrPropertyIntoClipboard()
         {
             GeminiUrl("object/object?object1=AdventureWorksModel.Product-990&object2=AdventureWorksModel.Customer-652");
             WaitForView(Pane.Left, PaneType.Object, "Mountain-500 Black, 42");
             WaitForView(Pane.Right, PaneType.Object, "Selected Distributors, AW00000652");
 
+            //Copy title from left pane
+            var title = WaitForCss("#pane1 .header .title");
+            title.Click();
+            CopyToClipboard(title);
+
+            //Copy title right pane
+            title = WaitForCss("#pane2 .header .title");
+            title.Click();
+            CopyToClipboard(title);
+
+            //Copy embedded reference from left pane
+            WaitForCss("#pane1 .header .title").Click();
+            var target = Tab(4);
+            Assert.AreEqual("Mountain-500", target.Text);
+            CopyToClipboard(target);
+
+            //Copy embedded reference from right pane
             WaitForCss("#pane2 .header .title").Click();
-            IWebElement active = br.SwitchTo().ActiveElement();
-            Assert.AreEqual("div", active.TagName);
-            Assert.AreEqual("Selected Distributors, AW00000652", active.Text);
-            active.SendKeys(Keys.Control + "c");
-            var copying = WaitForCss(".footer .currentclass .reference");
-            Assert.AreEqual("Selected Distributors, AW00000652", copying.Text);
-            
+            target = Tab(3);
+            Assert.AreEqual("Southeast", target.Text);
+            CopyToClipboard(target);
+        }
+                    [TestMethod]
+        public virtual void CopyListItemIntoClipboard()
+        {
+            GeminiUrl("list/list?menu1=SpecialOfferRepository&action1=CurrentSpecialOffers&menu2=PersonRepository&action2=ValidCountries");
+            WaitForView(Pane.Left, PaneType.List, "Current Special Offers");
+            WaitForView(Pane.Right, PaneType.List, "Valid Countries");
+
+            //Copy item from list, left pane
+            WaitForCss("#pane1 .header .title").Click();
+            var item = Tab(3);
+            Assert.AreEqual("No Discount", item.Text);
+            CopyToClipboard(item);
+
+            //Copy item from list, right pane
+
+            WaitForCss("#pane2 .header .menu").Click();
+            Assert.AreEqual("Actions", br.SwitchTo().ActiveElement().Text);
+
+             item = Tab(4);
+            Assert.AreEqual("Australia", item.Text);
+            CopyToClipboard(item);
+
         }
 
+        private void CopyToClipboard(IWebElement element)
+        {
+            var title = element.Text;
+            element.SendKeys(Keys.Control + "c");
+            wait.Until(dr => dr.FindElement(By.CssSelector(".footer .currentcopy .reference")).Text == title);
+        }
+
+        private IWebElement Tab(int numberIfTabs = 1)
+        {
+            for (int i = 1; i <= numberIfTabs; i++)
+            {
+                br.SwitchTo().ActiveElement().SendKeys(Keys.Tab);
+            }
+            return br.SwitchTo().ActiveElement();
+        }
     }
 
     #region browsers specific subclasses
