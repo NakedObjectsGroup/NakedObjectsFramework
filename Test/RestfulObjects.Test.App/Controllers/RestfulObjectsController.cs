c3694ca32cd83546a8f710e37137a429bd4576f9
scascarini <scascarini@nakedobjects.org>
2015-12-04 08:04:00 +0000

    fix build

--- Test/RestfulObjects.Test.App/Controllers/RestfulObjectsController.cs
+++ Test/RestfulObjects.Test.App/Controllers/RestfulObjectsController.cs
@@ -15,7 +15,7 @@ using RestfulObjects.Mvc.Model;
 
 namespace RestfulObjects.Test.App.Controllers {
     public class RestfulObjectsController : RestfulObjectsControllerBase {
-        public RestfulObjectsController(INakedObjectsSurface surface) : base(surface) {}
+        public RestfulObjectsController(INakedObjectsSurface surface, IOidStrategy oidStrategy) : base(surface, oidStrategy) {}
 
         [HttpGet]
         public override HttpResponseMessage GetHome([ModelBinder(typeof (ReservedArgumentsBinder))] ReservedArguments arguments) {
