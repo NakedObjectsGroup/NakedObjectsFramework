b9e9002a22ce176404033f3b1d36fa86cc4aee0c
scascarini <scascarini@nakedobjects.org>
2015-12-07 10:09:28 +0000

    fix karma test

--- Spa/NakedObjects.Spa/Tests/specs/nakedobjects.gemini.services.viewmodelfactory.test.ts
+++ Spa/NakedObjects.Spa/Tests/specs/nakedobjects.gemini.services.viewmodelfactory.test.ts
@@ -117,7 +117,7 @@ describe("nakedobjects.gemini.services.viewmodelfactory", () => {
         const rawAction = {
             extensions: {
                 friendlyName: "a title",
-                x_ro_nof_menuPath: "a path"
+                "x-ro-nof-menuPath": "a path"
             },
             links: [rawdetailsLink], 
             parameters: {} as _.Dictionary<NakedObjects.RoInterfaces.IParameterRepresentation>, 
