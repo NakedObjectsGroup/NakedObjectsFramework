ffee4f65f87d79e2c036da1cc25e0bc8c0217505
scascarini <scascarini@nakedobjects.org>
2016-01-05 13:46:47 +0000

    tidy karma

--- Spa/NakedObjects.Spa/Tests/specs/nakedobjects.gemini.test.ts
+++ Spa/NakedObjects.Spa/Tests/specs/nakedobjects.gemini.test.ts
@@ -12,7 +12,6 @@
 /// <reference path="../../Scripts/nakedobjects.gemini.services.urlmanager.ts" />
 /// <reference path="../../Scripts/nakedobjects.gemini.services.focusmanager.ts" />
 /// <reference path="../../Scripts/nakedobjects.gemini.services.viewmodelfactory.ts" />
-
 /// <reference path="nakedobjects.gemini.test.helpers.ts" />
 
 module NakedObjects.Gemini.Test {
@@ -21,13 +20,26 @@ module NakedObjects.Gemini.Test {
     import PaneRouteData = Angular.Gemini.PaneRouteData;
     import MenusViewModel = Angular.Gemini.MenusViewModel;
     import FocusTarget = Angular.Gemini.FocusTarget;
-    import ActionViewModel = NakedObjects.Angular.Gemini.ActionViewModel;
-    import DialogViewModel = NakedObjects.Angular.Gemini.DialogViewModel;
+    import ActionViewModel = Angular.Gemini.ActionViewModel;
+    import DialogViewModel = Angular.Gemini.DialogViewModel;
     describe("nakedobjects.gemini.tests", () => {
 
         beforeEach(angular.mock.module("app"));
 
         let $httpBackend: ng.IHttpBackendService;
+        let testRouteData: PaneRouteData;
+        let testScope: INakedObjectsScope;
+        let timeout: ng.ITimeoutService;
+
+        function setupBasePaneRouteData(paneId: number) {
+            const trd = new PaneRouteData(paneId);
+
+            trd.collections = {};
+            trd.parms = {};
+            trd.props = {};
+            trd.fields = {};
+            return trd;
+        }
 
         beforeEach(inject($injector => {
             // Set up the mock http service responses
@@ -35,22 +47,16 @@ module NakedObjects.Gemini.Test {
             Helpers.setupBackend($httpBackend);
         }));
 
+        beforeEach(inject(($rootScope: ng.IRootScopeService, $timeout: ng.ITimeoutService) => {
+            testScope = $rootScope.$new() as INakedObjectsScope;
+            testRouteData = setupBasePaneRouteData(1);
+            timeout = $timeout;
+        }));
+
         describe("Go to Home Page", () => {
-            let testRouteData: PaneRouteData;
-            let testScope: INakedObjectsScope;
+            
             let testEventSpy: jasmine.Spy;
-            let timeout: ng.ITimeoutService;
-
-            function setupBasePaneRouteData(paneId: number) {
-                const trd = new PaneRouteData(paneId);
-
-                trd.collections = {};
-                trd.parms = {};
-                trd.props = {};
-                trd.fields = {};
-                return trd;
-            }
-
+            
             function executeHandleHome(handlers: IHandlers) {
                 handlers.handleHome(testScope, testRouteData);
                 $httpBackend.flush();
@@ -66,7 +72,7 @@ module NakedObjects.Gemini.Test {
 
             function verifyOpenMenuHomePageState(ts: INakedObjectsScope) {
                 expect(ts.actionsTemplate).toBe(Angular.actionsTemplate);
-                expect((<{actions : ActionViewModel[]}>ts.object).actions.length).toBe(4);
+                expect((<{ actions: ActionViewModel[] }>ts.object).actions.length).toBe(4);
             }
 
             function verifyOpenDialogHomePageState(ts: INakedObjectsScope) {
@@ -76,8 +82,7 @@ module NakedObjects.Gemini.Test {
                 expect(dialogViewModel.title).toBe("Find Vendor By Account Number");
             }
 
-
-            function getEventTestFunc (t: FocusTarget, i: number, p: number, c: number) {              
+            function getEventTestFunc(t: FocusTarget, i: number, p: number, c: number) {
                 return (e, target: FocusTarget, index: number, paneId: number, count: number) => {
                     expect(e.name).toBe("geminiFocuson");
                     expect(target).toBe(t);
@@ -87,24 +92,20 @@ module NakedObjects.Gemini.Test {
                 }
             }
 
-            function setupEventSpy(ts : INakedObjectsScope,  target: FocusTarget, index: number, paneId: number, count: number) {
+            function setupEventSpy(ts: INakedObjectsScope, target: FocusTarget, index: number, paneId: number, count: number) {
                 const tes = jasmine.createSpy("event", getEventTestFunc(target, index, paneId, count));
                 tes.and.callThrough();
                 testScope.$on("geminiFocuson", tes);
                 return tes;
             }
 
-            beforeEach(inject(($rootScope: ng.IRootScopeService, $timeout: ng.ITimeoutService) => {
-                testScope = $rootScope.$new() as INakedObjectsScope;
-                testRouteData = setupBasePaneRouteData(1);
-                timeout = $timeout;
-            }));
-
-          
             describe("Without open menu or dialog", () => {
 
-                beforeEach(inject((handlers: IHandlers) => {
+                beforeEach(inject(() => {
                     testEventSpy = setupEventSpy(testScope, FocusTarget.Menu, 0, 1, 1);
+                }));
+
+                beforeEach(inject((handlers: IHandlers) => {
                     executeHandleHome(handlers);
                 }));
 
@@ -115,9 +116,12 @@ module NakedObjects.Gemini.Test {
 
             describe("With open menu", () => {
 
-                beforeEach(inject((handlers: IHandlers) => {
+                beforeEach(inject(() => {
                     testRouteData.menuId = "VendorRepository";
                     testEventSpy = setupEventSpy(testScope, FocusTarget.SubAction, 0, 1, 1);
+                }));
+
+                beforeEach(inject((handlers: IHandlers) => {
                     executeHandleHome(handlers);
                 }));
 
@@ -129,10 +133,13 @@ module NakedObjects.Gemini.Test {
 
             describe("With open menu and dialog", () => {
 
-                beforeEach(inject((handlers: IHandlers) => {
+                beforeEach(inject(() => {
                     testRouteData.menuId = "VendorRepository";
                     testRouteData.dialogId = "FindVendorByAccountNumber";
                     testEventSpy = setupEventSpy(testScope, FocusTarget.Dialog, 0, 1, 1);
+                }));
+
+                beforeEach(inject((handlers: IHandlers) => {
                     executeHandleHome(handlers);
                 }));
 
@@ -142,9 +149,6 @@ module NakedObjects.Gemini.Test {
                     verifyOpenDialogHomePageState(testScope);
                 });
             });
-
-
         });
-
     });
 }
