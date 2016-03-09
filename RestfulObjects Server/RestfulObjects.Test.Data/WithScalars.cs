a093c42e37eb508617e04b5e464bdf08ea51eaf3
scascarini <scascarini@nakedobjects.org>
2016-03-09 13:51:01 +0000

    fix end to end tests

--- RestfulObjects Server/RestfulObjects.Test.Data/WithScalars.cs
+++ RestfulObjects Server/RestfulObjects.Test.Data/WithScalars.cs
@@ -71,6 +71,7 @@ namespace RestfulObjects.Test.Data {
         public virtual DateTime DateTime { get; set; }
 
         [DataType(DataType.Password)]
+        [Column("Maptopassword")]
         public virtual string Password { get; set; }
 
         public virtual ICollection<MostSimple> List {
