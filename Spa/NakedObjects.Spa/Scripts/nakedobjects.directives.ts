1067b16e5634fc981edf8a5d7c8e7578ebfbaa82
scascarini <scascarini@nakedobjects.org>
2016-03-21 14:39:56 +0000

    support tristate bools

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.directives.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.directives.ts
@@ -631,4 +631,69 @@ module NakedObjects {
         }
     }));
 
+    app.directive("geminiBoolean", () => ({
+        require: "?ngModel",
+        link(scope: any, el: any, attrs: any, ctrl: any) {
+
+            const parent = scope.$parent as IPropertyOrParameterScope;
+            const viewModel = parent.parameter || parent.property;
+
+            ctrl.$formatters = [];
+            ctrl.$parsers = [];
+
+            ctrl.$validators.geminiBoolean = (modelValue: any, viewValue: string) => {
+                return viewModel.validate(modelValue, viewValue, true);
+            };
+
+            ctrl.$render = () => {
+                const d = ctrl.$viewValue as boolean;
+                el.data("checked", d);
+                switch (d) {
+                    case true:
+                        el.prop("indeterminate", false);
+                        el.prop("checked", true);
+                        break;
+                    case false:
+                        el.prop("indeterminate", false);
+                        el.prop("checked", false);
+                        break;
+                    default: // null
+                        el.prop("indeterminate", true);
+                }
+            };
+
+            const triStateClick = () => {
+                let d: boolean;
+                switch (el.data("checked")) {
+                case false:
+                    d = true;
+                    break;
+                case true:
+                    d = null;
+                    break;
+                default: // null
+                    d = false;
+                }
+                ctrl.$setViewValue(d);
+                scope.$apply(ctrl.$render);
+            };
+
+            const twoStateClick = () => {
+                let d: boolean;
+                switch (el.data("checked")) {
+                    case true:
+                        d = false;
+                        break;
+                    default: // false or null
+                        d = true;
+                }
+                ctrl.$setViewValue(d);
+                scope.$apply(ctrl.$render);
+            };
+
+            el.bind("click", viewModel.optional ? triStateClick : twoStateClick);
+        }
+    }));
+
+
 }
