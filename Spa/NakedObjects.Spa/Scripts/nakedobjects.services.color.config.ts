ebcf33e687a8fedd439141a0794b0c62296cd27a
Richard Pawson <rpawson@nakedobjects.net>
2016-03-29 15:29:52 +0100

    Re-define colours

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.services.color.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.services.color.config.ts
@@ -5,35 +5,21 @@
 module NakedObjects {
 
     app.run((color: IColor) => {
-        //color.addType("AdventureWorksModel.CustomerRepository", 17);
-        //color.addType("AdventureWorksModel.Individual", 1);
-        //color.addType("AdventureWorksModel.OrderRepository", 3);
-        //color.addType("AdventureWorksModel.SalesOrderDetail", 3);
-        //color.addType("AdventureWorksModel.ProductRepository", 8);
-        color.addType("AdventureWorksModel.Product", 1);
-        //color.addType("AdventureWorksModel.ProductInventory", 8);
-        //color.addType("AdventureWorksModel.ProductReview", 8);
-        //color.addType("AdventureWorksModel.ProductModel", 7);
-        //color.addType("AdventureWorksModel.ProductCategory", 17);
-        //color.addType("AdventureWorksModel.ProductSubCategory", 6);
-        //color.addType("AdventureWorksModel.EmployeeRepository", 0);
-        //color.addType("AdventureWorksModel.Employee", 2);
-        //color.addType("AdventureWorksModel.EmployeePayHistory", 0);
-        //color.addType("AdventureWorksModel.EmployeeDepartmentHistory",0);
-        //color.addType("AdventureWorksModel.SalesRepository", 13);
-        color.addType("AdventureWorksModel.SalesPerson", 4);
-        //color.addType("AdventureWorksModel.SpecialOfferRepository", 11);
-        color.addType("AdventureWorksModel.SpecialOffer", 5);
-        //color.addType("AdventureWorksModel.ContactRepository",16);
-        //color.addType("AdventureWorksModel.Contact", 16);
-        //color.addType("AdventureWorksModel.VendorRepository", 5);
-        color.addType("AdventureWorksModel.Vendor", 2);
-        //color.addType("AdventureWorksModel.PurchaseOrderRepository",14);
-        //color.addType("AdventureWorksModel.PurchaseOrder", 14);
-        //color.addType("AdventureWorksModel.WorkOrderRepository", 10);
-        color.addType("AdventureWorksModel.WorkOrder", 3);
-        //color.addType("AdventureWorksModel.OrderContributedActions", 18);
-        //color.addType("AdventureWorksModel.CustomerContributedActions", 18);
+
+        color.addType("AdventureWorksModel.Customer", 1);
+        color.addType("AdventureWorksModel.Individual", 1);
+        color.addType("AdventureWorksModel.Store", 1);
+        color.addType("AdventureWorksModel.SalesOrderHeader", 2);
+        color.addType("AdventureWorksModel.SalesOrderDetail", 3);
+        color.addType("AdventureWorksModel.Product", 4);
+        color.addType("AdventureWorksModel.Employee", 5);
+        color.addType("AdventureWorksModel.SalesPerson", 6);
+        color.addType("AdventureWorksModel.SpecialOffer", 7);
+        color.addType("AdventureWorksModel.Person", 8);
+        color.addType("AdventureWorksModel.Vendor", 9);
+        color.addType("AdventureWorksModel.PurchaseOrderHeader", 10);
+        color.addType("AdventureWorksModel.PurchaseOrderHeader", 11);
+        color.addType("AdventureWorksModel.WorkOrder", 12);
 
         //const matchBusiness = /AdventureWorksModel.Business*/;
 
