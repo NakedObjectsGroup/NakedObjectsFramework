eb2cc016a18b817d394bd1bc28d5c6d7bf8ef6ff
Richard Pawson <rpawson@nakedobjects.net>
2016-06-03 13:37:22 +0100

    New test for correct retention of dialog values

--- Spa/NakedObjects.Spa.Selenium.Test/tests/DialogTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/DialogTests.cs
@@ -44,14 +44,23 @@ namespace NakedObjects.Web.UnitTests.Selenium
             CancelDialog();
             WaitUntilElementDoesNotExist(".dialog");
         }
-        public virtual void ClosedDialogClearsFields()
+        public virtual void FieldsRetainedWhenTheyShouldbe()
         {
-            //To test: that when dialog is cancelled the fields
-            //have been cleared. 
-            GeminiUrl("home?m1=CustomerRepository&d1=FindIndividualCustomerByName&f1_firstName=%22a%22&f1_lastName=%22b%22");
-            wait.Until(d=>d.FindElement(By.CssSelector("#firstname1")).GetAttribute("value")== "a");
-            wait.Until(d => d.FindElement(By.CssSelector("#lastname1")).GetAttribute("value")== "b");
-            CancelDialog();
+            GeminiUrl("home?m1=CustomerRepository&d1=FindIndividualCustomerByName");
+            ClearFieldThenType("#firstname1", "arthur");
+            ClearFieldThenType("#lastname1", "brent");
+            //1. Navigating away and back retains values
+            ClickRecentButton();
+            WaitForView(Pane.Single, PaneType.Recent);
+            ClickBackButton();
+            WaitForView(Pane.Single, PaneType.Home);
+            wait.Until(d => d.FindElement(By.CssSelector("#firstname1")).GetAttribute("value") == "arthur");
+            wait.Until(d => d.FindElement(By.CssSelector("#lastname1")).GetAttribute("value") == "brent");
+            //2. Navigating away then opening dialog again should not retain values
+            ClickRecentButton();
+            WaitForView(Pane.Single, PaneType.Recent);
+            ClickBackButton();
+            WaitForView(Pane.Single, PaneType.Home);
             OpenSubMenu("Individuals");
             OpenActionDialog("Find Individual Customer By Name");
             wait.Until(d => d.FindElement(By.CssSelector("#firstname1")).GetAttribute("value") == "");
@@ -414,7 +423,7 @@ namespace NakedObjects.Web.UnitTests.Selenium
         [TestMethod]
         public override void TestCancelDialog() { base.TestCancelDialog(); }
         [TestMethod]
-        public override void ClosedDialogClearsFields() { base.ClosedDialogClearsFields(); }
+        public override void FieldsRetainedWhenTheyShouldbe() { base.FieldsRetainedWhenTheyShouldbe(); }
         [TestMethod]
         public override void ScalarParmShowsDefaultValue() { base.ScalarParmShowsDefaultValue(); }
         [TestMethod]
@@ -501,7 +510,7 @@ namespace NakedObjects.Web.UnitTests.Selenium
         }
     }
 
-    //[TestClass]
+    [TestClass]
     public class DialogTestsFirefox : DialogTests
     {
         [ClassInitialize]
@@ -563,7 +572,7 @@ namespace NakedObjects.Web.UnitTests.Selenium
             base.PasswordParam();
             base.ScalarChoicesParm();
             base.TestCancelDialog();
-            base.ClosedDialogClearsFields();
+            base.FieldsRetainedWhenTheyShouldbe();
             base.ScalarParmShowsDefaultValue();
             base.DateTimeParmKeepsValue();
             base.TimeSpanParm();
