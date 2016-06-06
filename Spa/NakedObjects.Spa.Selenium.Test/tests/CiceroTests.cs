8d78429b7287d1a9789ade61bcdaf60827a22501
Richard Pawson <rpawson@nakedobjects.net>
2016-06-06 16:07:09 +0100

    Ignore Cicero tests for the moment

--- Spa/NakedObjects.Spa.Selenium.Test/tests/CiceroTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/CiceroTests.cs
@@ -1423,7 +1423,7 @@ namespace NakedObjects.Web.UnitTests.Selenium
             base.ChainedCommands();
         }
     }
-    [TestClass]
+    [TestClass, Ignore]
     public class MegaCiceroTestsFirefox : MegaCiceroTestsRoot
     {
         [ClassInitialize]
