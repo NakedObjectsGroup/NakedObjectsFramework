d2ee2e0cef409889b8fa55a10318b48b352c9904
scascarini <scascarini@nakedobjects.org>
2016-06-14 16:23:57 +0100

    example code and changes for custom views and controllers

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
@@ -3,6 +3,11 @@
 
 module NakedObjects {
     app.run((template: ITemplate) => {
-       // template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/customObjectView.html");
+       //template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/openbydefaultObjectView.html");
+       //template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/withActionButtonObjectView.html");
+       //template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/withGroupedPropertiesObjectView.html");
+       //template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/withAccordionObjectView.html");
+       //template.setTemplateName("AdventureWorksModel.Product", InteractionMode.View, "Content/partials/pollingObjectView.html");
+
     });
-}
+} 
