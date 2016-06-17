d857ee92bfcb3d6e1b9af19d625d1b3336d925be
scascarini <scascarini@nakedobjects.org>
2016-06-17 15:22:41 +0100

    fix menu bug and update packages

--- Samples/AdventureWorksModel/Production/ProductInventory.cs
+++ Samples/AdventureWorksModel/Production/ProductInventory.cs
@@ -87,9 +87,9 @@ namespace AdventureWorksModel {
             menu.CreateSubMenu("Sub Menu")
                 .AddAction(nameof(Action1))
                 .CreateSubMenu("Level 2 sub menu")
-                .AddAction(nameof(Action2));
-                //.CreateSubMenu("Level 3 sub menu")
-                //.AddRemainingNativeActions();
+                .AddAction(nameof(Action2))
+                .CreateSubMenu("Level 3 sub menu")
+                .AddRemainingNativeActions();
         }
         public void Action1() { }
         public void Action2() { } 
