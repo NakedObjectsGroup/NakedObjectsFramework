5fd7031376e0dffc672f1444beb788e291c48ce0
Richard Pawson <rpawson@nakedobjects.net>
2016-06-17 15:50:23 +0100

    Example code for custom List view

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
@@ -8,6 +8,6 @@ module NakedObjects {
         //template.setTemplateName("AdventureWorksModel.Product", TemplateType.Object, InteractionMode.View, "Content/customTemplates/withGroupedPropertiesObjectView.html");
         //template.setTemplateName("AdventureWorksModel.Product", TemplateType.Object, InteractionMode.View, "Content/customTemplates/withAccordionObjectView.html");
         //template.setTemplateName("AdventureWorksModel.Product", TemplateType.Object, InteractionMode.View, "Content/customTemplates/pollingObjectView.html");
-
+        //template.setTemplateName("AdventureWorksModel.Product", TemplateType.List, CollectionViewState.List, "Content/customTemplates/productList.html");
     });
 }
