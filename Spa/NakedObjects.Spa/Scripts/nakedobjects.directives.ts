1b9fab3a131469c50566b3a15be417c8ee17f2e5
Richard Pawson <rpawson@nakedobjects.net>
2016-06-28 15:21:42 +0100

    Added css and commented-out directive code for clear button

--- Spa/NakedObjects.Spa/Scripts/nakedobjects.directives.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.directives.ts
@@ -909,3 +909,17 @@ module NakedObjects {
 
 
 }
+
+//new code for clear fields icon
+//jQuery(function ($) {
+
+//    function tog(v) { return v ? 'addClass' : 'removeClass'; }
+//    $(document).on('input', '.clearable', function () {
+//        $(this)[tog(this.value)]('x');
+//    }).on('mousemove', '.x', function (e) {
+//        $(this)[tog(this.offsetWidth - 18 < e.clientX - this.getBoundingClientRect().left)]('onX');
+//    }).on('touchstart click', '.onX', function (ev) {
+//        ev.preventDefault();
+//        $(this).removeClass('x onX').val('').change();
+//    });
+//});
