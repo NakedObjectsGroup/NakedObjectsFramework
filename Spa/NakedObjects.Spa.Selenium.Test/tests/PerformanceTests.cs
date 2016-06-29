0c4b4531920e249353f9880bc232587702010de4
Richard Pawson <rpawson@nakedobjects.net>
2016-06-29 17:20:23 +0100

    Remove performance tests firefox

--- Spa/NakedObjects.Spa.Selenium.Test/tests/PerformanceTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/PerformanceTests.cs
@@ -66,7 +66,7 @@ namespace NakedObjects.Selenium
         }
     }
 
-    [TestClass]
+    //[TestClass]
     public class PerformanceTestsFirefox : PerformanceTests
     {
         [ClassInitialize]
