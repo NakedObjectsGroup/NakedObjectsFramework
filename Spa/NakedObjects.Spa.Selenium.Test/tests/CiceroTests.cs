3b7fb0e52493d9a8da703911e24e38a46a5e99d2
scascarini <scascarini@nakedobjects.org>
2016-07-18 11:02:28 +0100

    temp ignore cicero tests

--- Spa/NakedObjects.Spa.Selenium.Test/tests/CiceroTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/CiceroTests.cs
@@ -1552,7 +1552,7 @@ namespace NakedObjects.Selenium {
         }
     }
 
-    [TestClass] 
+    //[TestClass] 
     public class MegaCiceroTestsChrome : MegaCiceroTestsRoot {
         [ClassInitialize]
         public new static void InitialiseClass(TestContext context) {
