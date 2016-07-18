3a40a18ae95f75233fc0a89dfced2e0d5ab6a519
scascarini <scascarini@nakedobjects.org>
2016-07-18 16:05:40 +0100

    save custom calendar work

---
+++ Spa/NakedObjects.Spa/Scripts/custom.directives.ts
@@ -0,0 +1,73 @@
+﻿/// <reference path="typings/angularjs/angular.d.ts" />
+/// <reference path="nakedobjects.models.ts" />
+/// <reference path="nakedobjects.services.viewmodelfactory.ts" />
+/// <reference path="nakedobjects.viewmodels.ts" />
+/// <reference path="typings/moment/moment.d.ts"/>
+
+module NakedObjects {
+    import Scope = angular.IScope;
+
+    app.directive("customFullcalendar", (mask: IMask, $timeout: ng.ITimeoutService): ng.IDirective => {
+        return {
+            // Enforce the angularJS default of restricting the directive to
+            // attributes only
+            restrict: "A",
+
+            // to make sure dynamic ids on element get picked up
+            transclude: true,
+            // This method needs to be defined and passed in from the
+            // passed in to the directive from the view controller
+
+            link(scope: Scope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController) {
+
+                const collection = (scope.$parent as INakedObjectsScope).collection;
+
+                const content: any = element.append("<div class='content'></div>");
+
+                let id: number = null;
+
+                function setup() {
+
+                    if (_.every(collection.items, i => i.tableRowViewModel)) {
+                        clearInterval(id);
+
+                        let lastDate: Date;
+
+                        const events = _.map(collection.items, i => {
+                                const date = i.tableRowViewModel.properties[2].value as Date;
+
+                                if (!lastDate || date > lastDate) {
+                                    lastDate = date;
+                                }
+
+                                return {
+                                    title: i.title,
+                                    start: date.toISOString(),
+                                    allDay: true,
+                                    color: (<any>i).color,
+                                    vm: i,
+                                    url: "empty"
+                                };
+                            });
+
+                        content.fullCalendar({
+                            events: events,
+                            eventClick: (evt: any) => {
+                                $timeout(() => evt.vm.doClick(false));
+                                return false;
+                            }
+                        });
+
+                        content.fullCalendar("gotoDate", lastDate.toISOString());                       
+                    }
+                }
+
+                id = setInterval(setup, 100);
+            }
+        };
+    });
+
+
+
+
+}
--- Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
@@ -6,5 +6,7 @@ module NakedObjects {
         template.setTemplateName("AdventureWorksModel.Location", TemplateType.Object, InteractionMode.View, "Content/customTemplates/locationView.html");
         template.setTemplateName("AdventureWorksModel.WorkOrderRouting", TemplateType.Object, InteractionMode.Edit, "Content/customTemplates/workOrderRoutingEdit.html");
         template.setTemplateName("AdventureWorksModel.Location", TemplateType.List, CollectionViewState.List, "Content/customTemplates/locationList.html");
+        //template.setTemplateName("AdventureWorksModel.SalesOrderHeader", TemplateType.List, CollectionViewState.Table, "Content/customTemplates/calendarList.html");
+
     });
 }
