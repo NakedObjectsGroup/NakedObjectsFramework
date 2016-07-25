0234236dab958d2ae84c3fc62a3433d8da481d98
Richard Pawson <rpawson@nakedobjects.net>
2016-07-25 12:58:23 +0100

    More work on calendar views

--- Spa/NakedObjects.Spa/Scripts/custom.directives.ts
+++ Spa/NakedObjects.Spa/Scripts/custom.directives.ts
@@ -67,7 +67,72 @@ namespace NakedObjects {
         };
     });
 
+    app.directive("customWorkordercalendar", (mask: IMask, $timeout: ng.ITimeoutService, urlManager : IUrlManager): ng.IDirective => {
+        return {
+            // Enforce the angularJS default of restricting the directive to
+            // attributes only
+            restrict: "A",
 
+            // to make sure dynamic ids on element get picked up
+            transclude: true,
+            // This method needs to be defined and passed in from the
+            // passed in to the directive from the view controller
+
+            link(scope: Scope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController) {
+
+                const object = (scope.$parent as INakedObjectsScope).object;
+                const collection = object.collections[0];
+                collection.doTable();
+                collection.refresh(urlManager.getRouteData().pane()[object.onPaneId], true);
+
+
+                const content: any = element.append("<div class='content'></div>");
+
+                let id: number = null;
+
+                function setup() {
+
+                    if (collection.items && _.every(collection.items, i => i.tableRowViewModel)) {
+                        clearInterval(id);
+
+                        let lastDate: Date;
+
+                        if (collection.items.length === 0) {
+                            lastDate = new Date(Date.now());
+                        }
+
+                        const events = _.map(collection.items, i => {
+                            const date = i.tableRowViewModel.properties[2].value as Date;
+
+                            if (!lastDate || date > lastDate) {
+                                lastDate = date;
+                            }
+
+                            return {
+                                title: i.title,
+                                start: date.toISOString(),
+                                allDay: true,
+                                color: (<any>i).color,
+                                vm: i,
+                                url: "empty"
+                            };
+                        });
+
+                        content.fullCalendar({
+                            events: events,
+                            eventClick: (evt: any) => {
+                                $timeout(() => evt.vm.doClick(false));
+                                return false;
+                            }
+                        });
+                        content.fullCalendar("gotoDate", lastDate.toISOString());
+                    }
+                }
+
+                id = setInterval(setup, 100);
+            }
+        };
+    });
 
 
 }
--- Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
+++ Spa/NakedObjects.Spa/Scripts/nakedobjects.services.template.config.ts
@@ -3,10 +3,10 @@
 
 namespace NakedObjects {
     app.run((template: ITemplate) => {
-        template.setTemplateName("AdventureWorksModel.Location", TemplateType.Object, InteractionMode.View, "Content/customTemplates/locationView.html");
-        template.setTemplateName("AdventureWorksModel.WorkOrderRouting", TemplateType.Object, InteractionMode.Edit, "Content/customTemplates/workOrderRoutingEdit.html");
-        template.setTemplateName("AdventureWorksModel.Location", TemplateType.List, CollectionViewState.List, "Content/customTemplates/locationList.html");
-        //template.setTemplateName("AdventureWorksModel.SalesOrderHeader", TemplateType.List, CollectionViewState.Table, "Content/customTemplates/calendarList.html");
+        //template.setTemplateName("AdventureWorksModel.Location", TemplateType.Object, InteractionMode.View, "Content/customTemplates/locationView.html");
+        //template.setTemplateName("AdventureWorksModel.WorkOrderRouting", TemplateType.Object, InteractionMode.Edit, "Content/customTemplates/workOrderRoutingEdit.html");
+        //template.setTemplateName("AdventureWorksModel.Location", TemplateType.List, CollectionViewState.List, "Content/customTemplates/locationList.html");
+        template.setTemplateName("AdventureWorksModel.WorkOrder", TemplateType.Object, InteractionMode.View, "Content/customTemplates/workOrderCalendarView.html");
 
     });
 }
