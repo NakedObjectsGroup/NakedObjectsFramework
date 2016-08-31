5015ff5282d694f86a91a2633d4114233c8c389d
Richard Pawson <rpawson@nakedobjects.net>
2016-08-31 11:44:16 +0100

    #39 AW model change to demo/test issue

--- Samples/AdventureWorksModel/Production/ProductRepository.cs
+++ Samples/AdventureWorksModel/Production/ProductRepository.cs
@@ -342,7 +342,13 @@ namespace AdventureWorksModel {
           public string ProductName { get; set; }
           public int Quantity { get; set; }
       }
-      
+
+
+        public IQueryable<ProductPhoto> AllProductPhotos()
+        {
+            return Container.Instances<ProductPhoto>();
+        }
+
     }
 
 }
