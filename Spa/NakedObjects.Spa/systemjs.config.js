3265aee74319964fd90992f7fb9e5b3083ad6720
scascarini <scascarini@nakedobjects.org>
2016-09-13 14:19:09 +0100

    config for dnd

--- Spa/NakedObjects.Spa/systemjs.config.js
+++ Spa/NakedObjects.Spa/systemjs.config.js
@@ -4,6 +4,8 @@
  */
 (function(global) {
 
+
+
   // map tells the System loader where to look for things
   var map = {
     'app':                        'app', // 'dist',
@@ -12,7 +14,8 @@
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs': 'node_modules/rxjs',
     'lodash': 'node_modules/lodash',
-    'moment': 'node_modules/moment'
+    'moment': 'node_modules/moment',
+    'ng2-dnd': 'node_modules/ng2-dnd'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -21,7 +24,8 @@
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
     'lodash': { main: 'index.js', defaultExtension: 'js' },
-    'moment': { main: 'moment.js', defaultExtension: 'js' }
+    'moment': { main: 'moment.js', defaultExtension: 'js' },
+    'ng2-dnd': { main: 'index.js', defaultExtension: 'js' }
   };
 
   var ngPackageNames = [
