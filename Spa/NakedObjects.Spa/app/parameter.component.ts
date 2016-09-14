a82094a883c96a2d6ac688c9dec8731dbb793679
scascarini <scascarini@nakedobjects.org>
2016-09-14 10:03:22 +0100

    add parameter componets

---
+++ Spa/NakedObjects.Spa/app/parameter.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActionComponent } from "./action.component";
+import { ViewModelFactory } from "./view-model-factory.service";
+import { UrlManager } from "./urlmanager.service";
+import * as Models from "./models";
+import * as ViewModels from "./nakedobjects.viewmodels";
+import * as Objectcomponent from './object.component';
+
+@Component({
+    selector: 'parameter',
+    templateUrl: 'app/parameter.component.html',
+    styleUrls: ['app/parameter.component.css']
+})
+export class ParameterComponent {
+
+    constructor(private viewModelFactory: ViewModelFactory, private urlManager: UrlManager) {}
+
+    parm: ViewModels.ParameterViewModel;
+
+    @Input()
+    edit: boolean;
+
+    @Input()
+    parent: ViewModels.DialogViewModel;
+
+    @Input()
+    set parameter(value: ViewModels.ParameterViewModel) {
+        this.parm = value;
+    }
+
+    get parameter() {
+        return this.parm;
+    }
+}
---
+++ Spa/NakedObjects.Spa/app/parameters.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, Input } from '@angular/core';
+import { ActionComponent } from "./action.component";
+import { ViewModelFactory } from "./view-model-factory.service";
+import { UrlManager } from "./urlmanager.service";
+import { PropertyComponent } from "./property.component";
+import * as Models from "./models";
+import * as ViewModels from "./nakedobjects.viewmodels";
+import * as Objectcomponent from './object.component';
+
+@Component({
+    selector: 'parameters',
+    templateUrl: 'app/parameters.component.html',
+    directives: [PropertyComponent],
+    styleUrls: ['app/parameters.component.css']
+})
+
+export class ParametersComponent {
+
+    constructor(private viewModelFactory: ViewModelFactory, private urlManager: UrlManager) { }
+
+    parms: ViewModels.ParameterViewModel[];
+
+    @Input()
+    edit: boolean;
+
+    @Input()
+    parent: ViewModels.DialogViewModel;
+
+    @Input()
+    set parameters(value: ViewModels.ParameterViewModel[]) {
+        this.parms = value;
+    }
+
+    get parameters() {
+        return this.parms;
+    }
+}
