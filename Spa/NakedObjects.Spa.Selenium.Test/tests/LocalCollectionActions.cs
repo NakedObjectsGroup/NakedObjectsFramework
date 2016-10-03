a251119230811cdaa25c9e6ff12010ff089c10ef
Richard Pawson <rpawson@nakedobjects.net>
2016-10-03 17:19:12 +0100

    #20 first selenium test

--- Spa/NakedObjects.Spa.Selenium.Test/tests/LocalCollectionActions.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/LocalCollectionActions.cs
@@ -11,7 +11,7 @@ using OpenQA.Selenium;
 
 namespace NakedObjects.Selenium {
     public abstract class LocalCollectionActionsTestsRoot : AWTest {
-        public void ActionsAndCheckBoxesVisible()
+        public virtual void ActionsAndCheckBoxesVisible()
         {
             GeminiUrl("object?i1=View&r=1&o1=___1.SalesOrderHeader--44284&c1_Details=List");
             //Test that first collection has two actions within it
@@ -19,12 +19,17 @@ namespace NakedObjects.Selenium {
             wait.Until(dr => dr.FindElements(By.CssSelector(".collection"))[0].FindElements(By.CssSelector(".action"))[0].Text == "Adjust Quantities");
             wait.Until(dr => dr.FindElements(By.CssSelector(".collection"))[0].FindElements(By.CssSelector(".action"))[1].Text == "Remove Details");
             //Confirm that checkboxes, including All are rendered
-
+            WaitForCss("input[type='checkbox']",17);
         }
     }
 
     public abstract class LocalCollectionActionsTests : LocalCollectionActionsTestsRoot {
      
+        [TestMethod]
+        public override void ActionsAndCheckBoxesVisible()
+        {
+            base.ActionsAndCheckBoxesVisible();
+        }
     }
 
     #region browsers specific subclasses
@@ -96,6 +101,7 @@ namespace NakedObjects.Selenium {
     public abstract class MegaLocalCollectionActionsTestsRoot : LocalCollectionActionsTestsRoot {
         [TestMethod] //Mega
         public void MegaLocalCollectionActionsTest() {
+            ActionsAndCheckBoxesVisible();
         }
     }
 
