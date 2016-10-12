6db8194108dacee7377a69e37cb7ee6b27907548
scascarini <scascarini@nakedobjects.org>
2016-10-12 11:22:40 +0100

    initial test work

--- Spa/NakedObjects.Spa.Selenium.Test/tests/FooterTests.cs
+++ Spa/NakedObjects.Spa.Selenium.Test/tests/FooterTests.cs
@@ -289,7 +289,7 @@ namespace NakedObjects.Selenium {
         }
     }
 
-    [TestClass, Ignore]
+    //[TestClass]
     public class MegaFooterTestsChrome : MegaFooterTestsRoot {
         [ClassInitialize]
         public new static void InitialiseClass(TestContext context) {
--- Spa/nakedobjectsspa/src/app/home/home.component.ts
+++ Spa/nakedobjectsspa/src/app/home/home.component.ts
@@ -14,7 +14,7 @@ import * as Models from "../models";
 import * as ViewModels from "../view-models";
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
