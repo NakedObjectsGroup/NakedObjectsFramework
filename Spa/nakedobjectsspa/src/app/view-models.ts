a07f8f0d1e26c60863b4b89abf0773c9bc1a7380
scascarini <scascarini@nakedobjects.org>
2016-10-14 16:11:06 +0100

    add checkboxes  to collections

--- Spa/nakedobjectsspa/src/app/view-models.ts
+++ Spa/nakedobjectsspa/src/app/view-models.ts
@@ -1285,6 +1285,8 @@ export class CollectionViewModel implements ICollectionViewModel {
 
     description = () => this.details.toString();
     refresh: (routeData: PaneRouteData, resetting: boolean) => void;
+
+    disableActions() { return true;}
 }
 
 export class MenusViewModel implements IMenusViewModel {
