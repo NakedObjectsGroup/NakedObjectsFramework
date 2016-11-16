c2effef1ecee2bd1f09935715a66db1f3eaea966
scascarini <scascarini@nakedobjects.org>
2016-11-16 14:58:38 +0000

    more component refactoring

--- Spa2/nakedobjectsspa/src/app/recent/recent.component.ts
+++ Spa2/nakedobjectsspa/src/app/recent/recent.component.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ISubscription } from 'rxjs/Subscription';
 import { UrlManagerService } from "../url-manager.service";
 import { RouteData, PaneRouteData } from "../route-data";
-import { RecentItemsViewModel} from '../view-models/recent-items-view-model';
+import { RecentItemsViewModel } from '../view-models/recent-items-view-model';
+import { RecentItemViewModel } from '../view-models/recent-item-view-model';
 import { PaneComponent } from '../pane/pane';
 
 @Component({
@@ -22,9 +23,21 @@ export class RecentComponent extends PaneComponent {
         super(activatedRoute, urlManager);
     }
 
-    vm: RecentItemsViewModel;
+    // template API 
+
+    title = "Recently Viewed Objects";
+    items = () => this.recent.items;
+
+    // todo again a smell - new child component ! 
+    itemColor = (item: RecentItemViewModel) => item.color;
+    itemTitle = (item: RecentItemViewModel) => item.title;
+    itemFriendlyName = (item: RecentItemViewModel) => item.friendlyName;
+
+    doItemClick = (item: RecentItemViewModel, right?: boolean) => item.doClick(right);
+
+    recent: RecentItemsViewModel;
 
     protected setup(routeData: PaneRouteData) {
-        this.vm = this.viewModelFactory.recentItemsViewModel(this.paneId);
+        this.recent = this.viewModelFactory.recentItemsViewModel(this.paneId);
     }
 }
