35661e8a2ce8cde315191c8cd1bdbe8e60242524
scascarini <scascarini@nakedobjects.org>
2017-01-05 09:55:10 +0000

    refactor multi line dialog

--- Spa2/nakedobjectsspa/src/app/view-models/multi-line-dialog-view-model.ts
+++ Spa2/nakedobjectsspa/src/app/view-models/multi-line-dialog-view-model.ts
@@ -1,4 +1,5 @@
 import { DialogViewModel } from './dialog-view-model';
+import { CollectionViewModel } from './collection-view-model';
 import { RouteData, PaneRouteData } from '../route-data';
 import { ColorService } from '../color.service';
 import { ContextService } from '../context.service';
@@ -10,7 +11,6 @@ import * as Ro from '../ro-interfaces';
 import * as Config from '../config';
 import * as Msg from '../user-messages';
 import * as _ from "lodash";
-import {CollectionViewModel} from './collection-view-model';
 
 export class MultiLineDialogViewModel {
 
@@ -20,112 +20,60 @@ export class MultiLineDialogViewModel {
         private readonly viewModelFactory: ViewModelFactoryService,
         private readonly urlManager: UrlManagerService,
         private readonly error: ErrorService,
-        private routeData: PaneRouteData,
-        private action: Models.IInvokableAction,
+        private readonly routeData: PaneRouteData,
+        private readonly action: Models.IInvokableAction,
         holder: Models.MenuRepresentation | Models.DomainObjectRepresentation | CollectionViewModel
     ) {
 
         if (holder instanceof Models.DomainObjectRepresentation) {
             this.objectTitle = holder.title();
             this.objectFriendlyName = holder.extensions().friendlyName();
-        } 
+        }
+            
+        const initialCount = action.extensions().multipleLines() || 1;
 
-        this.reset(routeData, action);
+        this.dialogs = _.map(_.range(initialCount), i => this.createRow(i));
+        this.title = this.dialogs[0].title;
+        this.action.parent.etagDigest = "*";
     }
 
-
-    private createRow(i: number) {
+    private readonly createRow = (i: number) => {
         const dialogViewModel = this.viewModelFactory.dialogViewModel(this.routeData, this.action as Models.IInvokableAction, null, true);
 
         dialogViewModel.actionViewModel.gotoResult = false;
-
-        dialogViewModel.doCloseKeepHistory = () => {
-            dialogViewModel.submitted = true;
-        };
-
-        dialogViewModel.doCloseReplaceHistory = () => {
-            dialogViewModel.submitted = true;
-        };
-
-        dialogViewModel.doCompleteButLeaveOpen = () => {
-            dialogViewModel.submitted = true;
-        };
-
+        dialogViewModel.doCloseKeepHistory = () => dialogViewModel.submitted = true;
+        dialogViewModel.doCloseReplaceHistory = () => dialogViewModel.submitted = true;
+        dialogViewModel.doCompleteButLeaveOpen = () => dialogViewModel.submitted = true;
         dialogViewModel.parameters.forEach(p => p.setAsRow(i));
-
         return dialogViewModel;
     }
 
     readonly objectFriendlyName = "";
     readonly objectTitle = "";
-    title = "";
+    readonly title : string;
+    readonly dialogs: DialogViewModel[];
 
-    reset(routeData: PaneRouteData, action: Models.IInvokableAction) {
+    readonly header = () => this.dialogs.length === 0 ? [] : _.map(this.dialogs[0].parameters, p => p.title);
 
-        this.action = action;
-        this.routeData = routeData;
-        this.action.parent.etagDigest = "*";
-
-        const initialCount = action.extensions().multipleLines() || 1;
-
-        this.dialogs = _.map(_.range(initialCount), (i) => this.createRow(i));
-        this.title = this.dialogs[0].title;
-        return this;
-    }
-
-    dialogs: DialogViewModel[] = [];
-
-    header() {
-        return this.dialogs.length === 0 ? [] : _.map(this.dialogs[0].parameters, p => p.title);
-    }
-
-    clientValid() {
-        return _.every(this.dialogs, d => d.clientValid());
-    }
-
-    tooltip() {
-        const tooltips = _.map(this.dialogs, (d, i) => `row: ${i} ${d.tooltip() || "OK"}`);
-        return _.reduce(tooltips, (s, t) => `${s}\n${t}`);
-    }
-
-    invokeAndAdd(index: number) {
+    readonly invokeAndAdd = (index: number) => {
         this.dialogs[index].doInvoke();
         this.context.clearDialogValues();
         return this.add(index);
     }
 
-    add(index: number) {
+    private readonly pushNewDialog = () => this.dialogs.push(this.createRow(this.dialogs.length)) - 1;
+
+    private readonly add = (index: number) => {
         if (index === this.dialogs.length - 1) {
             // if this is last dialog always add another
-            return this.dialogs.push(this.createRow(this.dialogs.length)) - 1;
+            return this.pushNewDialog();
         }
         else if (_.takeRight(this.dialogs)[0].submitted) {
             // if the last dialog is submitted add another 
-            return this.dialogs.push(this.createRow(this.dialogs.length)) - 1;
+            return this.pushNewDialog();
         }
         return 0;
     }
 
-    clear(index: number) {
-        _.pullAt(this.dialogs, [index]);
-    }
-
-    submitAll() {
-        if (this.clientValid()) {
-            _.each(this.dialogs, d => {
-                if (!d.submitted) {
-                    d.doInvoke();
-                }
-            });
-        }
-    }
-
-    submittedCount() {
-        return _.filter(this.dialogs, d => d.submitted).length;
-    }
-
-    close() {
-        this.urlManager.popUrlState();
-    }
-
+    readonly submittedCount = () => _.filter(this.dialogs, d => d.submitted).length;
 }
