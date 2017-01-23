f34886bf5e453ad14311e1222a4bf69e314c8333
scascarini <scascarini@nakedobjects.org>
2017-01-23 16:16:49 +0000

    hook up custom component service

--- Spa2/nakedobjectsspa/src/app/dynamic-object/dynamic-object.component.ts
+++ Spa2/nakedobjectsspa/src/app/dynamic-object/dynamic-object.component.ts
@@ -1,21 +1,53 @@
 import { Component, OnInit, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
 import { ObjectComponent } from '../object/object.component';
+import { CustomComponentService } from '../custom-component.service';
+import { ActivatedRoute } from '@angular/router';
+import { ISubscription } from 'rxjs/Subscription';
+import { RouteData, PaneRouteData } from "../route-data";
+import { UrlManagerService } from "../url-manager.service";
+import * as Models from '../models';
+import * as Pane from '../pane/pane';
 
 @Component({
     selector: 'nof-dynamic-object',
     templateUrl: './dynamic-object.component.html',
     styleUrls: ['./dynamic-object.component.css']
 })
-export class DynamicObjectComponent implements OnInit {
+export class DynamicObjectComponent extends  Pane.PaneComponent {
 
     @ViewChild('parent', { read: ViewContainerRef })
     parent: ViewContainerRef;
 
-    constructor(private readonly componentFactoryResolver: ComponentFactoryResolver) {
+    constructor(
+        activatedRoute: ActivatedRoute,
+        urlManager: UrlManagerService,
+        private readonly componentFactoryResolver: ComponentFactoryResolver,
+        private readonly customComponentService: CustomComponentService) {
+        super(activatedRoute, urlManager);
     }
 
-    ngOnInit() {
-        const childComponent = this.componentFactoryResolver.resolveComponentFactory(ObjectComponent);
-        this.parent.createComponent(childComponent);
+   
+    private lastOid : string;
+
+    protected setup(routeData: PaneRouteData) {
+        if (!routeData.objectId) {
+            return;
+        }
+        const oid = Models.ObjectIdWrapper.fromObjectId(routeData.objectId);
+
+        if (oid.domainType !== this.lastOid) {
+            this.lastOid = oid.domainType;
+            this.parent.clear();
+
+            this.customComponentService.getCustomObjectComponent(oid).then(c => {
+                const childComponent = this.componentFactoryResolver.resolveComponentFactory(c);
+                this.parent.createComponent(childComponent);
+            });
+        }
+    }
+    
+    ngOnDestroy(): void {  
+       super.ngOnDestroy();
+        this.parent.clear();
     }
 }
