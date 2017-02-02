c10272f537603ab5ea44f784cd613f077c5c8107
scascarini <scascarini@nakedobjects.org>
2017-02-02 10:41:12 +0000

    start clearing todos

--- Spa2/nakedobjectsspa/src/app/app.component.ts
+++ Spa2/nakedobjectsspa/src/app/app.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
-
-    backgroundColor: string;
-
-    ngOnInit(): void {
-        // todo
-        this.backgroundColor = "object-color0";
-    }
+export class AppComponent {
 }
