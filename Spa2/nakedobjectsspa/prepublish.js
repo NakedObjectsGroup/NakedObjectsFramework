04e002dfffe4c57deaac57ad667e1f64a6644ad9
scascarini <scascarini@nakedobjects.org>
2017-02-20 16:01:14 +0000

    packaging to npm now works

--- Spa2/nakedobjectsspa/prepublish.js
+++ Spa2/nakedobjectsspa/prepublish.js
@@ -1,15 +1,18 @@
 ﻿var cpx = require('cpx');
 var replace = require('replace-in-file');
+var mv = require('mv');
 
 var copyTemplates = "./src/**/*.{html,css,png,eot,svg,ttf,woff,txt}";
 var copyBoilerPlate = "./src/app/app*.ts";
-var copyConfig = "./src/config.json";
+var copyConfig = "./src/empty_config.json";
 var copyToLib = "lib";
 var copyToApp = "lib/app";
 
 cpx.copySync(copyTemplates, copyToLib);
 cpx.copySync(copyConfig, copyToLib);
 cpx.copySync(copyBoilerPlate, copyToApp);
+
+mv("./lib/empty_config.json", "./lib/config.json", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
 
 var options = {
     files: ["./lib/app/app-routing.module.ts", "./lib/app/app.module.ts"],
@@ -18,7 +21,7 @@ var options = {
 };
 
 try {
-    let changedFiles = replace.sync(options);
+    var changedFiles = replace.sync(options);
     //console.log('Modified files:', changedFiles.join(', '));
 }
 catch (error) {
