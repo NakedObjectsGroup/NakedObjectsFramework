8e7a036a2e7f965d4911998f206689c0b5e67eea
scascarini <scascarini@nakedobjects.org>
2017-02-20 10:51:20 +0000

    add build scripts

---
+++ Spa2/nakedobjectsspa/postinstall.js
@@ -0,0 +1,20 @@
+﻿var gentlyCopy = require('gently-copy');
+
+var appfileList = [   
+    './lib/app/app-routing.module.ts',
+    './lib/app/app.component.css',
+    './lib/app/app.component.html',
+    './lib/app/app.component.ts',
+    './lib/app/app.module.ts'];
+
+var rootfileList = [
+    './lib/config.json',
+    './lib/index.html',
+    './lib/styles.css',
+    './lib/assets'];
+
+var rootdest = '../../';
+var appdest = '../../app/';
+
+gentlyCopy(appfileList, appdest, { overwrite: true });
+gentlyCopy(rootfileList, rootdest, { overwrite: true });
---
+++ Spa2/nakedobjectsspa/prepublish.js
@@ -0,0 +1,11 @@
+﻿var cpx = require('cpx');
+
+var copyTemplates = "./src/**/*.{html,css,png}";
+var copyBoilerPlate = "./src/app/app*.ts";
+var copyConfig = "./src/config.json";
+var copyToLib = "lib";
+var copyToApp = "lib/app";
+
+cpx.copy(copyTemplates, copyToLib);
+cpx.copy(copyConfig, copyToLib);
+cpx.copy(copyBoilerPlate, copyToApp);
