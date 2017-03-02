b788b5c5b9ec46d396de77b2eb531220ee935a36
scascarini <scascarini@nakedobjects.org>
2017-03-02 13:23:05 +0000

    more action rework

--- Spa2/nakedobjectsspa/src/app/action-bar/action-bar.component.ts
+++ Spa2/nakedobjectsspa/src/app/action-bar/action-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IActionHolder } from '../action/action.component';
+import { IMenuHolderViewModel} from '../view-models/imenu-holder-view-model';
+import { MenuItemViewModel} from '../view-models/menu-item-view-model';
 
 @Component({
     selector: 'nof-action-bar',
@@ -11,4 +13,24 @@ export class ActionBarComponent {
     @Input()
     actions: IActionHolder[];
 
+    @Input()
+    set menuHolder (mh : IMenuHolderViewModel) {
+
+        // todo DRY 
+
+        const menuItems = mh.menuItems;
+        const acts = _.flatten(_.map(menuItems, (mi: MenuItemViewModel) => mi.actions!));
+
+        this.actions = _.map(acts,
+            a => ({
+                value: a.title,
+                doClick: () => a.doInvoke(),
+                doRightClick: () => a.doInvoke(true),
+                show: () => true,
+                disabled: () => a.disabled() ? true : null,
+                tempDisabled: () => a.tempDisabled(),
+                title: () => a.description,
+                accesskey: null
+            })) as IActionHolder[];
+    }
 }
