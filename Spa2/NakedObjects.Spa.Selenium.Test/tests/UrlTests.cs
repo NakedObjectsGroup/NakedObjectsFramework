4b3fa5d109fba71939cb37f685ef70ea1348bae0
scascarini <scascarini@nakedobjects.org>
2017-03-03 13:39:21 +0000

    fix tests

--- Spa2/NakedObjects.Spa.Selenium.Test/tests/UrlTests.cs
+++ Spa2/NakedObjects.Spa.Selenium.Test/tests/UrlTests.cs
@@ -33,8 +33,8 @@ namespace NakedObjects.Selenium {
         public virtual void HomeWithMenu() {
             GeminiUrl("home?m1=CustomerRepository");
             WaitForView(Pane.Single, PaneType.Home, "Home");
-            wait.Until(d => d.FindElement(By.CssSelector(".actions")));
-            ReadOnlyCollection<IWebElement> actions = br.FindElements(By.CssSelector("nof-action"));
+            wait.Until(d => d.FindElement(By.CssSelector("nof-action-list")));
+            ReadOnlyCollection<IWebElement> actions = br.FindElements(By.CssSelector("nof-action-list nof-action"));
             Assert.AreEqual(CustomerServiceActions, actions.Count);
             //Assert.AreEqual("Find Customer By Account Number", actions[0].Text);
             //Assert.AreEqual("Find Store By Name", actions[1].Text);
