a588c7b1774bfd7e20ce5c73f3869d1711a9fc44
scascarini <scascarini@nakedobjects.org>
2017-03-08 10:10:04 +0000

    update package dependencies

--- Spa2/nakedobjectsspa/src/app/constants.ts
+++ Spa2/nakedobjectsspa/src/app/constants.ts
@@ -50,5 +50,5 @@ export const nofMessages = "x-ro-nof-messages";
 export const supportedDateFormats = ["D/M/YYYY", "D/M/YY", "D MMM YYYY", "D MMMM YYYY", "D MMM YY", "D MMMM YY"];
 
 // updated by build do not update manually or change name or regex may not match 
-export const clientVersion = 'unknown/development build';
+export const clientVersion = '9.0.0-beta.1';
 
