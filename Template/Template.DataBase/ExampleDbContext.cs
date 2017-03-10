440af8222184f662a8e7886424daf72a02855b6b
Richard Pawson <rpawson@nakedobjects.net>
2017-03-10 16:52:33 +0000

    Template project  -  split into multi-project solution

---
+++ Template/Template.DataBase/ExampleDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Data.Entity;
+using Template.Model;
+
+namespace Template.DataBase
+{
+    public class ExampleDbContext : DbContext
+    {
+        public ExampleDbContext(string dbName) : base(dbName)
+        {
+            Database.SetInitializer<DbContext>(new DropCreateDatabaseIfModelChanges<DbContext>());
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+}
---
+++ Template/Template.Server/App_Start/CorsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityModel.Http.Cors.WebApi;
+
+namespace Template.Server
+{
+    public class CorsConfig
+    {
+        public static void RegisterCors(HttpConfiguration httpConfig)
+        {
+            var corsConfig = new WebApiCorsConfiguration();
+            corsConfig.RegisterGlobal(httpConfig);
+            corsConfig.ForResources("RestfulObjects").
+                ForOrigins("http://localhost:5001").
+               AllowAll().
+               AllowResponseHeaders("Warning", "Set-Cookie", "ETag").
+               AllowCookies();
+
+        }
+    }
+}
