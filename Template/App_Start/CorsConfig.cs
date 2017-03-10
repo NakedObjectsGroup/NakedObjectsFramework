f0fcad9f55cce0069e08db170a4838a4f808255d
Richard Pawson <rpawson@nakedobjects.net>
2017-03-10 14:34:03 +0000

    Added CORS config to Template project

---
+++ Template/App_Start/CorsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityModel.Http.Cors.WebApi;
+
+namespace NakedObjects.Template
+{
+    public class CorsConfig
+    {
+        public static void RegisterCors(HttpConfiguration httpConfig)
+        {
+            var corsConfig = new WebApiCorsConfiguration();
+            corsConfig.RegisterGlobal(httpConfig);
+            corsConfig.ForResources("RestfulObjects").
+                ForOrigins("http://localhost:49998").
+               AllowAll().
+               AllowResponseHeaders("Warning", "Set-Cookie", "ETag").
+               AllowCookies();
+
+        }
+    }
+}
--- Template/App_Start/RestfulObjectsActivator.cs
+++ Template/App_Start/RestfulObjectsActivator.cs
@@ -4,9 +4,9 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
-
 using log4net.Config;
 using NakedObjects.Template;
+using System.Web.Http;
 using System.Web.Routing;
 using WebActivatorEx;
 
@@ -17,6 +17,7 @@ using WebActivatorEx;
 namespace NakedObjects.Template {
     public static class RestfulObjectsActivator {
         public static void PreStart() {
+            GlobalConfiguration.Configure(CorsConfig.RegisterCors);
             RestfulObjectsConfig.RestPreStart();
             RestfulObjectsConfig.RegisterRestfulObjectsRoutes(RouteTable.Routes);
         }
