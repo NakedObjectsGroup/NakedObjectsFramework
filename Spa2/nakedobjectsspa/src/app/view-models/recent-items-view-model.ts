e244c16545cf45e8f46ea4049625968ebc8b5029
scascarini <scascarini@nakedobjects.org>
2017-05-25 10:04:23 +0100

    make recent items update dynamically

--- Spa2/nakedobjectsspa/src/app/view-models/recent-items-view-model.ts
+++ Spa2/nakedobjectsspa/src/app/view-models/recent-items-view-model.ts
@@ -2,27 +2,55 @@
 import { ContextService } from '../context.service';
 import { ViewModelFactoryService } from '../view-model-factory.service';
 import map from 'lodash/map';
-
+import every from 'lodash/map';
+import * as Models from '../models';
 import { UrlManagerService } from '../url-manager.service';
 import { Pane } from '../route-data';
 
 export class RecentItemsViewModel {
 
     constructor(
-        viewModelFactory: ViewModelFactoryService,
+        private readonly viewModelFactory: ViewModelFactoryService,
         private readonly context: ContextService,
         private readonly urlManager: UrlManagerService,
         private readonly onPaneId: Pane
     ) {
-        const items = map(this.context.getRecentlyViewed(), (o, i) => ({ obj: o, link: o.updateSelfLinkWithTitle(), index: i }));
-        this.items = map(items, i => viewModelFactory.recentItemViewModel(i.obj, i.link!, onPaneId, false, i.index));
+        this.recentlyViewed = this.context.getRecentlyViewed();
+        this.refreshItems();
     }
 
-    items: RecentItemViewModel[];
+    private recentlyViewed : Models.DomainObjectRepresentation[];
+
+    private refreshItems() {       
+        const items = map(this.recentlyViewed, (o, i) => ({ obj: o, link: o.updateSelfLinkWithTitle(), index: i }));
+        this._items = map(items, i => this.viewModelFactory.recentItemViewModel(i.obj, i.link!, this.onPaneId, false, i.index));
+    }
+
+    private itemsHaveChanged() {
+        const currentRecentlyViewed = this.context.getRecentlyViewed();
+
+        const same = this.recentlyViewed.length === currentRecentlyViewed.length && 
+                     every(this.recentlyViewed, (v, i) => v.id() === currentRecentlyViewed[i].id());
+
+        if (!same) {
+            this.recentlyViewed = currentRecentlyViewed;
+        }
+
+        return !same;
+    }
+
+    private _items : RecentItemViewModel[];
+
+    get items() : RecentItemViewModel[] {
+        if (this.itemsHaveChanged()) {
+            this.refreshItems();
+        }
+        return this._items;
+    }
 
     readonly clear = () => {
         this.context.clearRecentlyViewed();
-        this.items = [];
+        this._items = [];
         this.urlManager.triggerPageReloadByFlippingReloadFlagInUrl(this.onPaneId);
     }
 }
