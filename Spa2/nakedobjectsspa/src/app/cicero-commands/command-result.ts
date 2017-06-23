f47549f490bda2427444843e135259d20ad1ab78
scascarini <scascarini@nakedobjects.org>
2017-06-23 15:04:45 +0100

    #68 cicero multi choices work

--- Spa2/nakedobjectsspa/src/app/cicero-commands/command-result.ts
+++ Spa2/nakedobjectsspa/src/app/cicero-commands/command-result.ts
@@ -1,23 +1,48 @@
+import { Dictionary } from 'lodash';
+import map from 'lodash/map';
+import mapValues from 'lodash/mapValues';
+import { Result } from './result';
+import { ContextService } from '../context.service';
 import * as Ro from '../models';
 import * as Models from '../models';
-import { ContextService } from '../context.service';
-import { Dictionary } from 'lodash';
-import mapValues from 'lodash/mapValues';
-import {Result} from './result';
 
 // todo move this 
-export function getParametersAndCurrentValue(action: Ro.ActionMember | Models.ActionRepresentation | Models.InvokableActionMember, context: ContextService): Dictionary<Ro.Value> {
+export function getParametersAndCurrentValue(action: Ro.ActionMember | Models.ActionRepresentation | Models.InvokableActionMember, context: ContextService):  Dictionary<Ro.Value> {
 
     if (action instanceof Models.InvokableActionMember || action instanceof Models.ActionRepresentation) {
         const parms = action.parameters();
-        const values = context.getDialogCachedValues(action.actionId());
-        return mapValues(parms, p => {
-            const value = values[p.id()];
+        const cachedValues = context.getDialogCachedValues(action.actionId());
+        const values = mapValues(parms, p => {
+            const value = cachedValues[p.id()];
             return value === undefined ? p.default() : value;
         });
+      
+        return  values;
     }
     return {};
 }
+
+export function getFields(field: Models.IField):  Models.IField[] {
+
+    if (field instanceof Models.Parameter) {
+        const action = field.parent;
+        if (action instanceof Models.InvokableActionMember || action instanceof Models.ActionRepresentation) {
+            const parms = action.parameters();
+            return map(parms, p => p as Models.IField);
+        }
+    }
+
+    if (field instanceof Models.PropertyMember) {
+        // todo 
+        return [];
+    }
+
+
+    return [];
+}
+
+
+
 
 export class CommandResult extends Result{
     changeState: () => void = () => { };
