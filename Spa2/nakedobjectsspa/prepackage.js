9e61d19800439b27854998e9606d29f3650d2201
scascarini <scascarini@nakedobjects.org>
2018-06-19 14:11:02 +0100

    add missing file

---
+++ Spa2/nakedobjectsspa/prepackage.js
@@ -0,0 +1,26 @@
+﻿/* eslint-disable */
+
+var replace = require('replace-in-file');
+var find = require('find-in-files');
+
+// get current version
+
+var version = find.findSync("version", ".", "package.json").then(s => {
+
+    try {
+        var versionLine = s["package.json"].line[0];
+        var versionSplit = versionLine.split('"');
+        var version = versionSplit[3];
+
+        // to update client version in code
+        var options2 = {
+            files: ["./src/app/constants.ts"],
+            from: [/clientVersion.*/g],
+            to: "clientVersion = '" + version + "';"
+        };
+
+        replace.sync(options2);
+    } catch (e) {
+        console.error('Error occurred updating version:', e);
+    }
+});
