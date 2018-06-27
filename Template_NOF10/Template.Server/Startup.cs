32b9d4627797e83c5ad122108a9ea6d82164b475
scascarini <scascarini@nakedobjects.org>
2018-06-27 19:05:51 +0100

    More auth work

--- Template_NOF10/Template.Server/Startup.cs
+++ Template_NOF10/Template.Server/Startup.cs
@@ -15,18 +15,20 @@ namespace Template.Server {
             var domain = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
             var apiIdentifier = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
 
-            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
-            app.UseJwtBearerAuthentication(
-                new JwtBearerAuthenticationOptions {
-                    AuthenticationMode = AuthenticationMode.Active,
-                    TokenValidationParameters = new TokenValidationParameters() {
-                        // causes this clain to be used as 'Identity.Name'
-                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
-                        ValidAudience = apiIdentifier,
-                        ValidIssuer = domain,
-                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
-                    }
-                });
+            if (apiIdentifier != "") {
+                var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
+                app.UseJwtBearerAuthentication(
+                    new JwtBearerAuthenticationOptions {
+                        AuthenticationMode = AuthenticationMode.Active,
+                        TokenValidationParameters = new TokenValidationParameters() {
+                            // causes this claim to be used as 'Identity.Name'
+                            NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+                            ValidAudience = apiIdentifier,
+                            ValidIssuer = domain,
+                            IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+                        }
+                    });
+            }
 
             // Configure Web API
             WebApiConfig.Configure(app);
