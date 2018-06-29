c4a64e84704fbbb6a29c8fe546f3e1d0e3669dd8
scascarini <scascarini@nakedobjects.org>
2018-06-29 17:35:12 +0100

    more schematics work

--- Spa2/nakedobjects-schematics/prebuild.js
+++ Spa2/nakedobjects-schematics/prebuild.js
@@ -1,8 +1,8 @@
 var mv = require('mv');
 
-mv("../nakedobjectsspa/src/app/app.component.ts", "./src/files/code/component", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
-mv("../nakedobjectsspa/src/app/app.component.css", "./src/files/code/component_css", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
-mv("../nakedobjectsspa/src/app/app.component.html", "./src/files/code/component_template", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
-mv("../nakedobjectsspa/src/app/app.module.ts", "./src/files/code/module", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
-mv("../nakedobjectsspa/src/app/app-routing.module.ts", "./src/files/code/routing", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
+mv("../nakedobjectsspa/src/app/app.component.ts", "./src/nakedobjects-schematics/files/code/component", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
+mv("../nakedobjectsspa/src/app/app.component.css", "./src/nakedobjects-schematics/files/code/component_css", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
+mv("../nakedobjectsspa/src/app/app.component.html", "./src/nakedobjects-schematics/files/code/component_template", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
+mv("../nakedobjectsspa/src/app/app.module.ts", "./src/nakedobjects-schematics/files/code/module", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
+mv("../nakedobjectsspa/src/app/app-routing.module.ts", "./src/nakedobjects-schematics/files/code/routing", { mkdirp: false }, function (err) { if (err) console.error('Error occurred:', err); });
 
