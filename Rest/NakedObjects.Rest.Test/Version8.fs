01d8486dc3ac00cdd7feb58f913c62d0a82ebd9d
scascarini <scascarini@nakedobjects.org>
2018-08-02 20:05:17 +0100

    starting fixing tests

--- Rest/NakedObjects.Rest.Test/Version8.fs
+++ Rest/NakedObjects.Rest.Test/Version8.fs
@@ -32,7 +32,7 @@ let links =
 let expected = 
     [ TProperty(JsonPropertyNames.Links, links)
       TProperty(JsonPropertyNames.SpecVersion, TObjectVal("1.1"))
-      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("8.0.1\r\n"))
+      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("10.0.0-beta01\r\n"))
       TProperty(JsonPropertyNames.OptionalCapabilities, capabilities)
       TProperty(JsonPropertyNames.Extensions, TObjectJson([])) ]
 
