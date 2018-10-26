c8721da9580008c511e439cf837b5d86576b2e5e
scascarini <scascarini@nakedobjects.org>
2018-10-26 12:48:41 +0100

    Return to old reflector

--- Demo/NakedObjects.Rest.App.DemoOwin/App_Start/UnityConfig.cs
+++ Demo/NakedObjects.Rest.App.DemoOwin/App_Start/UnityConfig.cs
@@ -31,6 +31,7 @@ namespace NakedObjects.Rest.App.DemoOwin {
         private static readonly Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() => {
             var container = new UnityContainer();
             RegisterTypes(container);
+            //RegisterParallelTypes(container);
             return container;
         });
 
@@ -46,7 +47,7 @@ namespace NakedObjects.Rest.App.DemoOwin {
         /// <param name="container">The unity container to configure.</param>
         /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
         /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
-        public static void RegisterTypes(IUnityContainer container) {
+        public static void RegisterParallelTypes(IUnityContainer container) {
             // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
             // container.LoadConfiguration();
             //Standard configuration
@@ -59,6 +60,35 @@ namespace NakedObjects.Rest.App.DemoOwin {
 
             UnityConfigHelpers.RegisterFacetFactory(typeof(AWNotNavigableFacetFactoryParallel), container, 1000);
             UnityConfigHelpers.RegisterFacetFactory(typeof(AWNotCountedAnnotationFacetFactoryParallel), container, 1001);
+
+            // config 
+            container.RegisterInstance<IReflectorConfiguration>(NakedObjectsRunSettings.ReflectorConfig(), (new ContainerControlledLifetimeManager()));
+            container.RegisterInstance<IEntityObjectStoreConfiguration>(NakedObjectsRunSettings.EntityObjectStoreConfig(), new ContainerControlledLifetimeManager());
+
+            // surface
+            container.RegisterType<IOidTranslator, OidTranslatorSlashSeparatedTypeAndIds>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<IOidStrategy, EntityOidStrategy>(new HierarchicalLifetimeManager());
+            container.RegisterType<IStringHasher, SimpleStringHasher>(new HierarchicalLifetimeManager());
+            container.RegisterType<IFrameworkFacade, FrameworkFacade>(new HierarchicalLifetimeManager());
+
+            //Externals
+            container.RegisterType<IPrincipal>(new InjectionFactory(c => HttpContext.Current.User));
+        }
+
+        public static void RegisterTypes(IUnityContainer container) {
+            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
+            // container.LoadConfiguration();
+            //Standard configuration
+
+            StandardUnityConfig.RegisterStandardFacetFactories(container);
+            StandardUnityConfig.RegisterCoreContainerControlledTypes(container);
+            StandardUnityConfig.RegisterCorePerTransactionTypes<HierarchicalLifetimeManager>(container);
+
+            // add custom notnavigable factory
+
+            UnityConfigHelpers.RegisterFacetFactory(typeof(AWNotNavigableFacetFactory), container, 1000);
+            UnityConfigHelpers.RegisterFacetFactory(typeof(AWNotCountedAnnotationFacetFactory), container, 1001);
 
             // config 
             container.RegisterInstance<IReflectorConfiguration>(NakedObjectsRunSettings.ReflectorConfig(), (new ContainerControlledLifetimeManager()));
