d0027116fb5cb1dc6727db8ac80a9de2d31f4238
scascarini <scascarini@nakedobjects.org>
2019-03-20 17:44:01 +0000

    update test

--- Test/NakedObjects.Rest.Test.EndToEnd/PropertyDelete.cs
+++ Test/NakedObjects.Rest.Test.EndToEnd/PropertyDelete.cs
@@ -48,7 +48,7 @@ namespace RestfulObjects.Test.EndToEnd {
             Helpers.TestResponse(refProp, FilePrefix + "Before", newRef, Methods.Put, Codes.Succeeded);
         }
 
-        [Ignore]
+        //[Ignore]
         [TestMethod]
         public void DeleteValueProperty() {
             Helpers.TestResponse(valueProp, FilePrefix + "After-ValueProperty", null, Methods.Delete);
