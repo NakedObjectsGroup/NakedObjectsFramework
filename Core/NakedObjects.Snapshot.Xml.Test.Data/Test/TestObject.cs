0fee2764ad7b978911ed303840e87f10f47ce3aa
scascarini <scascarini@nakedobjects.org>
2020-02-10 15:34:54 +0000

    tidy xml test code

--- Core/NakedObjects.Snapshot.Xml.Test.Data/Test/TestObject.cs
+++ Core/NakedObjects.Snapshot.Xml.Test.Data/Test/TestObject.cs
@@ -1,5 +1,5 @@
 // Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,40 +12,19 @@ using NakedObjects;
 
 namespace Snapshot.Xml.Test {
     public class TestObjectContext : DbContext {
-        public TestObjectContext(string name) : base(name) {}
+        public TestObjectContext(string name) : base(name) { }
         public DbSet<TestObject> TestObjects { get; set; }
     }
 
     public class TestObject {
-        private IList<TestObject> testCollection = new List<TestObject>();
-
         [Key]
         [Hidden(WhenTo.Always)]
         public virtual int TestObjectId { get; set; }
-
-        //Add properties with 'propv', collections with 'coll', actions with 'act' shortcuts
 
         public virtual int TestInt { get; set; }
         public virtual string TestString { get; set; }
         public virtual TestObject TestReference { get; set; }
 
-        public virtual IList<TestObject> TestCollection {
-            get { return testCollection; }
-            set { testCollection = value; }
-        }
-
-        #region Injected Services
-
-        // This region should contain properties to hold references to any services required by the
-        // object.  Use the 'injs' shortcut to add a new service; 'injc' to add an injected Container
-
-        #endregion
-
-        #region Life Cycle Methods
-
-        // This region should contain any of the 'life cycle' convention methods (such as
-        // Created(), Persisted() etc) called by the framework at specified stages in the lifecycle.
-
-        #endregion
+        public virtual IList<TestObject> TestCollection { get; set; } = new List<TestObject>();
     }
 }
--- Core/NakedObjects.Snapshot.Xml.Test.Data/Test/TransformRepository.cs
+++ Core/NakedObjects.Snapshot.Xml.Test.Data/Test/TransformRepository.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -10,13 +10,7 @@ using Snapshot.Xml.Test.Two;
 using TransformFull = Snapshot.Xml.Test.One.TransformFull;
 
 namespace Snapshot.Xml.Test {
-    //[Named("")]
     public class TransformRepository : AbstractFactoryAndRepository {
-        // 'fact' shortcut to add a factory method, 
-        // 'alli' for an all-instances method
-        // 'find' for a method to find a single object by query
-        // 'list' for a method to return a list of objects matching a query
-
         public TransformFull TransformFull() {
             var obj = NewTransientInstance<TransformFull>();
 
@@ -42,12 +36,5 @@ namespace Snapshot.Xml.Test {
 
             return obj;
         }
-
-        #region Injected Services
-
-        // This region should contain properties to hold references to any services required by the
-        // object.  Use the 'injs' shortcut to add a new service.
-
-        #endregion
     }
 }
