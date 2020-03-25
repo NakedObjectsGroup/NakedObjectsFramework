45a23ffe76b1a1563d288a5f814c9ff48c1ee2fe
scascarini <scascarini@nakedobjects.org>
2020-03-25 09:52:13 +0000

    Update TestTypes.fs

--- Core/NakedObjects.Persistor.Entity.Test/TestTypes.fs
+++ Core/NakedObjects.Persistor.Entity.Test/TestTypes.fs
@@ -19,7 +19,7 @@ open System.Reflection
 
 // connection strings 
 
-#if APPVEYOR 
+//#if APPVEYOR 
 
 let csAW = @"Data Source=.\SQL2017;Initial Catalog=AdventureWorks;Integrated Security=True;"
 let csAWMARS = @"Data source=.\SQL2017;initial catalog=AdventureWorks;integrated security=True;MultipleActiveResultSets=True;"
@@ -30,18 +30,18 @@ let csCOCE = @"Data Source=.\SQL2017;Initial Catalog=CodeOnlyCeTests;Integrated 
 let csCS = @"Data Source=.\SQL2017;Initial Catalog=CodeSystemTest;Integrated Security=True;"
 let csTDCO = @"Data Source=.\SQL2017;Initial Catalog=TestDataCodeOnly;Integrated Security=True;"
 
-#else
+//#else
 
-let csAW = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks;Integrated Security=True;"
-let csAWMARS = @"Data source=.\SQLEXPRESS;initial catalog=AdventureWorks;integrated security=True;MultipleActiveResultSets=True;"
-let csMD = @"Data Source=.\SQLEXPRESS;Initial Catalog=AMultiDatabaseTests;Integrated Security=True;"
-let csMF = @"Data Source=.\SQLEXPRESS;Initial Catalog=ModelFirst;Integrated Security=True;"
-let csCO = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeOnlyTests;Integrated Security=True;"
-let csCOCE = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeOnlyCeTests;Integrated Security=True;"
-let csCS = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeSystemTest;Integrated Security=True;"
-let csTDCO = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestDataCodeOnly;Integrated Security=True;"
+//let csAW = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks;Integrated Security=True;"
+//let csAWMARS = @"Data source=.\SQLEXPRESS;initial catalog=AdventureWorks;integrated security=True;MultipleActiveResultSets=True;"
+//let csMD = @"Data Source=.\SQLEXPRESS;Initial Catalog=AMultiDatabaseTests;Integrated Security=True;"
+//let csMF = @"Data Source=.\SQLEXPRESS;Initial Catalog=ModelFirst;Integrated Security=True;"
+//let csCO = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeOnlyTests;Integrated Security=True;"
+//let csCOCE = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeOnlyCeTests;Integrated Security=True;"
+//let csCS = @"Data Source=.\SQLEXPRESS;Initial Catalog=CodeSystemTest;Integrated Security=True;"
+//let csTDCO = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestDataCodeOnly;Integrated Security=True;"
 
-#endif
+//#endif
 
 
 let injectedObjects = new List<Object>()
