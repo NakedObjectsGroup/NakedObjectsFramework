3fc3514163f379b98a39a98fc5ace5b0f9aa3613
scascarini <scascarini@nakedobjects.org>
2020-03-25 09:21:54 +0000

    Update XmlSnapshotService.fs

--- Core/NakedObjects.Snapshot.Xml.Test/XmlSnapshotService.fs
+++ Core/NakedObjects.Snapshot.Xml.Test/XmlSnapshotService.fs
@@ -22,12 +22,12 @@ open System.IO
 open System.Text.RegularExpressions
 open System.Xml.Linq
 
-#if APPVEYOR 
+//#if APPVEYOR 
 //let cs = @"Server=(local)\SQL2017;Database=TestObject;User ID=sa;Password=Password12!"
 let cs = @"Data Source=.\SQL2017;Initial Catalog=TestObject;Integrated Security=True;"
-#else
-let cs = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestObject;Integrated Security=True;"
-#endif
+//#else
+//let cs = @"Data Source=.\SQLEXPRESS;Initial Catalog=TestObject;Integrated Security=True;"
+//#endif
 
 let normalizeData d1 d2 =
     // ignore keys 
