6a14d7aefa32992347a0042d99080c835d59b9ea
Richard Pawson <rpawson@nakedobjects.net>
2020-09-15 15:47:10 +0100

    AW added the (included) mappings back into the context

--- Samples/AdventureWorksFunctionalModel/AdventureWorksContext.cs
+++ Samples/AdventureWorksFunctionalModel/AdventureWorksContext.cs
@@ -58,9 +58,9 @@ namespace AdventureWorksModel
         //public DbSet<PurchaseOrderHeader> PurchaseOrderHeaders { get; set; }
         //public DbSet<ShipMethod> ShipMethods { get; set; }
         //public DbSet<Vendor> Vendors { get; set; }
-        //public DbSet<PersonCreditCard> PersonCreditCards { get; set; }
+        public DbSet<PersonCreditCard> PersonCreditCards { get; set; }
         //public DbSet<CountryRegionCurrency> CountryRegionCurrencies { get; set; }
-        //public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
         public DbSet<Currency> Currencies { get; set; }
         //public DbSet<CurrencyRate> CurrencyRates { get; set; }
         //public DbSet<Customer> Customers { get; set; }
@@ -78,77 +78,77 @@ namespace AdventureWorksModel
         public DbSet<SpecialOfferProduct> SpecialOfferProducts { get; set; }
         //public DbSet<Store> Stores { get; set; }
 
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Configurations.Add(new DepartmentMap());
-        //    modelBuilder.Configurations.Add(new EmployeeMap());
-        //    modelBuilder.Configurations.Add(new EmployeeAddressMap());
-        //    modelBuilder.Configurations.Add(new EmployeeDepartmentHistoryMap());
-        //    modelBuilder.Configurations.Add(new EmployeePayHistoryMap());
-        //    modelBuilder.Configurations.Add(new JobCandidateMap());
-        //    modelBuilder.Configurations.Add(new ShiftMap());
-        //    modelBuilder.Configurations.Add(new AddressMap());
-        //    modelBuilder.Configurations.Add(new AddressTypeMap());
-        //    modelBuilder.Configurations.Add(new BusinessEntityMap());
-        //    modelBuilder.Configurations.Add(new BusinessEntityAddressMap());
-        //    modelBuilder.Configurations.Add(new BusinessEntityContactMap());
-        //    modelBuilder.Configurations.Add(new PersonMap());
-        //    modelBuilder.Configurations.Add(new ContactTypeMap());
-        //    modelBuilder.Configurations.Add(new CountryRegionMap());
-        //    modelBuilder.Configurations.Add(new EmailAddressMap());
-        //    modelBuilder.Configurations.Add(new PasswordMap());
-        //    modelBuilder.Configurations.Add(new PersonPhoneMap());
-        //    modelBuilder.Configurations.Add(new PhoneNumberTypeMap());
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new DepartmentMap());
+            //modelBuilder.Configurations.Add(new EmployeeMap());
+            //modelBuilder.Configurations.Add(new EmployeeAddressMap());
+            //modelBuilder.Configurations.Add(new EmployeeDepartmentHistoryMap());
+            //modelBuilder.Configurations.Add(new EmployeePayHistoryMap());
+            //modelBuilder.Configurations.Add(new JobCandidateMap());
+            modelBuilder.Configurations.Add(new ShiftMap());
+            modelBuilder.Configurations.Add(new AddressMap());
+            modelBuilder.Configurations.Add(new AddressTypeMap());
+            modelBuilder.Configurations.Add(new BusinessEntityMap());
+            modelBuilder.Configurations.Add(new BusinessEntityAddressMap());
+            modelBuilder.Configurations.Add(new BusinessEntityContactMap());
+            modelBuilder.Configurations.Add(new PersonMap());
+            modelBuilder.Configurations.Add(new ContactTypeMap());
+            modelBuilder.Configurations.Add(new CountryRegionMap());
+            modelBuilder.Configurations.Add(new EmailAddressMap());
+            modelBuilder.Configurations.Add(new PasswordMap());
+            modelBuilder.Configurations.Add(new PersonPhoneMap());
+            modelBuilder.Configurations.Add(new PhoneNumberTypeMap());
 
-        //    modelBuilder.Configurations.Add(new StateProvinceMap());
-        //    modelBuilder.Configurations.Add(new BillOfMaterialMap());
-        //    modelBuilder.Configurations.Add(new CultureMap());
-        //    modelBuilder.Configurations.Add(new DocumentMap());
-        //    modelBuilder.Configurations.Add(new IllustrationMap());
-        //    modelBuilder.Configurations.Add(new LocationMap());
-        //    modelBuilder.Configurations.Add(new ProductMap());
-        //    modelBuilder.Configurations.Add(new ProductCategoryMap());
-        //    modelBuilder.Configurations.Add(new ProductCostHistoryMap());
-        //    modelBuilder.Configurations.Add(new ProductDescriptionMap());
-        //    modelBuilder.Configurations.Add(new ProductDocumentMap());
-        //    modelBuilder.Configurations.Add(new ProductInventoryMap());
-        //    modelBuilder.Configurations.Add(new ProductListPriceHistoryMap());
-        //    modelBuilder.Configurations.Add(new ProductModelMap());
-        //    modelBuilder.Configurations.Add(new ProductModelIllustrationMap());
-        //    modelBuilder.Configurations.Add(new ProductModelProductDescriptionCultureMap());
-        //    modelBuilder.Configurations.Add(new ProductPhotoMap());
-        //    modelBuilder.Configurations.Add(new ProductProductPhotoMap());
-        //    modelBuilder.Configurations.Add(new ProductReviewMap());
-        //    modelBuilder.Configurations.Add(new ProductSubcategoryMap());
-        //    modelBuilder.Configurations.Add(new ScrapReasonMap());
-        //    modelBuilder.Configurations.Add(new TransactionHistoryMap());
-        //    modelBuilder.Configurations.Add(new UnitMeasureMap());
-        //    modelBuilder.Configurations.Add(new WorkOrderMap());
-        //    modelBuilder.Configurations.Add(new WorkOrderRoutingMap());
-        //    modelBuilder.Configurations.Add(new ProductVendorMap());
-        //    modelBuilder.Configurations.Add(new PurchaseOrderDetailMap());
-        //    modelBuilder.Configurations.Add(new PurchaseOrderHeaderMap());
-        //    modelBuilder.Configurations.Add(new ShipMethodMap());
-        //    modelBuilder.Configurations.Add(new VendorMap());
-        //    modelBuilder.Configurations.Add(new PersonCreditCardMap());
-        //    modelBuilder.Configurations.Add(new CountryRegionCurrencyMap());
-        //    modelBuilder.Configurations.Add(new CreditCardMap());
-        //    modelBuilder.Configurations.Add(new CurrencyMap());
-        //    modelBuilder.Configurations.Add(new CurrencyRateMap());
-        //    modelBuilder.Configurations.Add(new CustomerMap());
-        //    modelBuilder.Configurations.Add(new SalesOrderDetailMap());
-        //    modelBuilder.Configurations.Add(new SalesOrderHeaderMap());
-        //    modelBuilder.Configurations.Add(new SalesOrderHeaderSalesReasonMap());
-        //    modelBuilder.Configurations.Add(new SalesPersonMap());
-        //    modelBuilder.Configurations.Add(new SalesPersonQuotaHistoryMap());
-        //    modelBuilder.Configurations.Add(new SalesReasonMap());
-        //    modelBuilder.Configurations.Add(new SalesTaxRateMap());
-        //    modelBuilder.Configurations.Add(new SalesTerritoryMap());
-        //    modelBuilder.Configurations.Add(new SalesTerritoryHistoryMap());
-        //    modelBuilder.Configurations.Add(new ShoppingCartItemMap());
-        //    modelBuilder.Configurations.Add(new SpecialOfferMap());
-        //    modelBuilder.Configurations.Add(new SpecialOfferProductMap());
-        //    modelBuilder.Configurations.Add(new StoreMap());
-        //}
+            modelBuilder.Configurations.Add(new StateProvinceMap());
+            modelBuilder.Configurations.Add(new BillOfMaterialMap());
+            modelBuilder.Configurations.Add(new CultureMap());
+            modelBuilder.Configurations.Add(new DocumentMap());
+            modelBuilder.Configurations.Add(new IllustrationMap());
+            modelBuilder.Configurations.Add(new LocationMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new ProductCategoryMap());
+            modelBuilder.Configurations.Add(new ProductCostHistoryMap());
+            modelBuilder.Configurations.Add(new ProductDescriptionMap());
+            modelBuilder.Configurations.Add(new ProductDocumentMap());
+            modelBuilder.Configurations.Add(new ProductInventoryMap());
+            modelBuilder.Configurations.Add(new ProductListPriceHistoryMap());
+            modelBuilder.Configurations.Add(new ProductModelMap());
+            modelBuilder.Configurations.Add(new ProductModelIllustrationMap());
+            modelBuilder.Configurations.Add(new ProductModelProductDescriptionCultureMap());
+            modelBuilder.Configurations.Add(new ProductPhotoMap());
+            modelBuilder.Configurations.Add(new ProductProductPhotoMap());
+            modelBuilder.Configurations.Add(new ProductReviewMap());
+            modelBuilder.Configurations.Add(new ProductSubcategoryMap());
+            modelBuilder.Configurations.Add(new ScrapReasonMap());
+            modelBuilder.Configurations.Add(new TransactionHistoryMap());
+            modelBuilder.Configurations.Add(new UnitMeasureMap());
+            modelBuilder.Configurations.Add(new WorkOrderMap());
+            modelBuilder.Configurations.Add(new WorkOrderRoutingMap());
+            //modelBuilder.Configurations.Add(new ProductVendorMap());
+            //modelBuilder.Configurations.Add(new PurchaseOrderDetailMap());
+            //modelBuilder.Configurations.Add(new PurchaseOrderHeaderMap());
+            //modelBuilder.Configurations.Add(new ShipMethodMap());
+            //modelBuilder.Configurations.Add(new VendorMap());
+            modelBuilder.Configurations.Add(new PersonCreditCardMap());
+           // modelBuilder.Configurations.Add(new CountryRegionCurrencyMap());
+            modelBuilder.Configurations.Add(new CreditCardMap());
+            modelBuilder.Configurations.Add(new CurrencyMap());
+            //modelBuilder.Configurations.Add(new CurrencyRateMap());
+            //modelBuilder.Configurations.Add(new CustomerMap());
+            //modelBuilder.Configurations.Add(new SalesOrderDetailMap());
+            //modelBuilder.Configurations.Add(new SalesOrderHeaderMap());
+            //modelBuilder.Configurations.Add(new SalesOrderHeaderSalesReasonMap());
+            //modelBuilder.Configurations.Add(new SalesPersonMap());
+            //modelBuilder.Configurations.Add(new SalesPersonQuotaHistoryMap());
+            //modelBuilder.Configurations.Add(new SalesReasonMap());
+            //modelBuilder.Configurations.Add(new SalesTaxRateMap());
+            modelBuilder.Configurations.Add(new SalesTerritoryMap());
+            //modelBuilder.Configurations.Add(new SalesTerritoryHistoryMap());
+            //modelBuilder.Configurations.Add(new ShoppingCartItemMap());
+            modelBuilder.Configurations.Add(new SpecialOfferMap());
+            modelBuilder.Configurations.Add(new SpecialOfferProductMap());
+            //modelBuilder.Configurations.Add(new StoreMap());
+        }
     }
 }
