212cb78c76ac6b772c07823b26ac6b9e961d7a40
Stefano Cascarini <stef.cascarini@googlemail.com>
2020-11-02 17:18:12 +0000

    Add new programming model dll

--- Core Programming Model/NakedCore.ProgrammingModel/Menu/IMenu.cs
+++ Core Programming Model/NakedCore.ProgrammingModel/Menu/IMenu.cs
@@ -9,38 +9,38 @@ using System;
 
 namespace NakedObjects.Menu {
     /// <summary>
-    /// Provides methods for constructing a menu on a domain object. Implementation of this
-    /// interface will be provided by the framework.
+    ///     Provides methods for constructing a menu on a domain object. Implementation of this
+    ///     interface will be provided by the framework.
     /// </summary>
     public interface IMenu {
         /// <summary>
-        /// The type on which the actions to be added are defined. Will normally be the
-        /// object on which the Menu is defined, but may be changed to allow actions
-        /// to be added from another service.
+        ///     The type on which the actions to be added are defined. Will normally be the
+        ///     object on which the Menu is defined, but may be changed to allow actions
+        ///     to be added from another service.
         /// </summary>
         /// <param name="name"></param>
         /// <returns>This menu (for fluent programming)</returns>
         Type Type { get; set; }
 
         /// <summary>
-        /// Allows the default name for the menu to be over-ridden
-        /// The type on which the actions to be added are defined. Will normally be the
-        /// object on which the Menu is defined, but may be changed to allow actions
-        /// to be added from another service.
+        ///     Allows the default name for the menu to be over-ridden
+        ///     The type on which the actions to be added are defined. Will normally be the
+        ///     object on which the Menu is defined, but may be changed to allow actions
+        ///     to be added from another service.
         /// </summary>
         /// <param name="name"></param>
         /// <returns>This menu (for fluent programming)</returns>
         IMenu WithMenuName(string name);
 
         /// <summary>
-        /// Allows the id for the menu to be specified or over-ridden.
+        ///     Allows the id for the menu to be specified or over-ridden.
         /// </summary>
         /// <param name="name"></param>
         /// <returns>This menu (for fluent programming)</returns>
         IMenu WithId(string id);
 
         /// <summary>
-        /// Add an action from the Type.
+        ///     Add an action from the Type.
         /// </summary>
         /// <param name="actionName"></param>
         /// <param name="renamedTo"></param>
@@ -50,27 +50,29 @@ namespace NakedObjects.Menu {
         /// <summary>
         /// </summary>
         /// <param name="subMenuName"></param>
-        /// <returns>The new menu, which will already have been added to the hosting menu. It will
-        /// have the same Type as the super-menu.</returns>
+        /// <returns>
+        ///     The new menu, which will already have been added to the hosting menu. It will
+        ///     have the same Type as the super-menu.
+        /// </returns>
         IMenu CreateSubMenu(string subMenuName);
 
         /// <summary>
-        /// Finds a previously-created sub menu.  Throws exception if sub-menu of that name does not exist.
+        ///     Finds a previously-created sub menu.  Throws exception if sub-menu of that name does not exist.
         /// </summary>
         /// <param name="menuName"></param>
         /// <returns></returns>
         IMenu GetSubMenu(string menuName);
 
         /// <summary>
-        /// Adds all native actions from the Default Type not previously added directly or to a sub-menu.
+        ///     Adds all native actions from the Default Type not previously added directly or to a sub-menu.
         /// </summary>
         /// <returns>This menu (for fluent programming)</returns>
         IMenu AddRemainingNativeActions();
 
         /// <summary>
-        /// Adds all actions contributed to the Default Type.
-        /// Where a sub-menu exists of the correct name, actions will be added to that.
-        /// Will throw an exception if SetDefaultType has not been called.
+        ///     Adds all actions contributed to the Default Type.
+        ///     Where a sub-menu exists of the correct name, actions will be added to that.
+        ///     Will throw an exception if SetDefaultType has not been called.
         /// </summary>
         /// <returns></returns>
         IMenu AddContributedActions();
@@ -80,6 +82,5 @@ namespace NakedObjects.Menu {
         IMenu AddAllActions(Type fromType);
         IMenu AddRemainingActions(Type fromType);
         IMenu WithDefaultType(Type defaultType);
-
     }
 }
---
+++ Core Programming Model/NakedCore.ProgrammingModel/Menu/IMenuFactory.cs
@@ -0,0 +1,24 @@
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+
+using System;
+
+namespace NakedObjects.Menu {
+    public interface IMenuFactory {
+        //Creates a new menu with the Type specified. If the optional name
+        //parameter is not specified, then the menu takes its name from the type.
+        IMenu NewMenu<T>(bool addAllActions = false, string name = null);
+
+        //Creates a new menu with the Type specified. If the optional name
+        //parameter is not specified, then the menu takes its name from the type.
+        IMenu NewMenu(Type type, bool addAllActions = false, string name = null);
+
+        IMenu NewMenu(string name, string id);
+        IMenu NewMenu(string name, string id, Type defaultType, bool addAllActions = false);
+    }
+}
