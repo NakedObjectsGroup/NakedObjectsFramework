8742b15056f9f033524c54ead7aedc6aaf59622b
Stefano Cascarini <stef.cascarini@googlemail.com>
2020-12-02 17:01:33 +0000

    Update DisableForContextViaFunctionFacetTest.cs

--- Core/NakedObjects.DependencyInjection.Test/Extensions/DisableForContextViaFunctionFacetTest.cs
+++ Core/NakedObjects.DependencyInjection.Test/Extensions/DisableForContextViaFunctionFacetTest.cs
@@ -5,6 +5,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NakedObjects.DependencyInjection.Extensions;
@@ -16,6 +17,35 @@ namespace NakedObjects.DependencyInjection.Test.Extensions {
         public void TestFramework() {
             IServiceCollection services = new ServiceCollection();
             services.AddNakedFramework(options => { });
+        }
+
+        [TestMethod]
+        public void TestNakedObjects() {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNakedFramework(options => { options.AddNakedObjects(options => { options.Types = Array.Empty<Type>(); }); });
+        }
+
+        [TestMethod]
+        public void TestNakedFunctions() {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNakedFramework(options => { options.AddNakedFunctions(options => { options.Functions = Array.Empty<Type>(); }); });
+        }
+
+        [TestMethod]
+        public void TestRestfulObjects()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNakedFramework(options => { options.AddRestfulObjects(options => { options.AcceptHeaderStrict = true; }); });
+        }
+
+        [TestMethod]
+        public void TestAll() {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNakedFramework(options => {
+                options.AddNakedObjects(options => { options.Types = Array.Empty<Type>(); });
+                options.AddNakedFunctions(options => { options.Functions = Array.Empty<Type>(); });
+                options.AddRestfulObjects(options => { options.AcceptHeaderStrict = true; });
+            });
         }
     }
 }
