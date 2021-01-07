ffb67541b4fb46b65beb9f24e42d2770851ded73
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-01-07 13:45:55 +0000

    #200 add test project

--- NakedFunctions/NakedFunctions.Services.Test/TestClock.cs
+++ NakedFunctions/NakedFunctions.Services.Test/TestClock.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace NakedFunctions.Services.Test
-{
+namespace NakedFunctions.Services.Test {
     [TestClass]
-    public class TestClock
-    {
-
+    public class TestClock {
         [TestMethod]
-        public void TestNow()
-        {
+        public void TestNow() {
             var c = new Clock();
             var actual = c.Now();
             var expected = DateTime.Now;
@@ -20,8 +23,7 @@ namespace NakedFunctions.Services.Test
         }
 
         [TestMethod]
-        public void TestToday()
-        {
+        public void TestToday() {
             var c = new Clock();
             Assert.AreEqual(DateTime.Today, c.Today());
         }
--- NakedFunctions/NakedFunctions.Services.Test/TestGuidGenerator.cs
+++ NakedFunctions/NakedFunctions.Services.Test/TestGuidGenerator.cs
@@ -1,23 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace NakedFunctions.Services.Test
-{
+namespace NakedFunctions.Services.Test {
     [TestClass]
-    public class TestGuidGenerator
-    {
-
+    public class TestGuidGenerator {
         [TestMethod]
-        public void Test1()
-        {
+        public void Test1() {
             IGuidGenerator gen = new GuidGenerator();
-            Guid g1 = gen.NewGuid();
+            var g1 = gen.NewGuid();
             Assert.AreNotEqual(Guid.Empty, g1);
             Assert.AreNotEqual(Guid.NewGuid(), g1);
 
-            Guid g2 = gen.NewGuid();
+            var g2 = gen.NewGuid();
             Assert.AreNotEqual(g1, g2);
-
         }
     }
 }
