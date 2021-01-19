903f53e0a3b7fd956354cab6cb3bfb3fcd2cdc64
Richard Pawson <rpawson@nakedobjects.net>
2021-01-19 12:43:54 +0000

    AW Model - continue re-introducing Choices functions

--- Test/AdventureWorksFunctionalModel/Production/Product_Functions.cs
+++ Test/AdventureWorksFunctionalModel/Production/Product_Functions.cs
@@ -12,21 +12,23 @@ using NakedFunctions;
 using AW.Types;
 using static AW.Helpers;
 
-namespace AW.Functions {
-    public static class Product_Functions {
+namespace AW.Functions
+{
+    public static class Product_Functions
+    {
 
         #region BestSpecialOffer
         [DescribedAs("Determines the best discount offered by current special offers for a specified order quantity")]
         public static SpecialOffer BestSpecialOffer(
-            this Product p,[ValueRange(1,999)] int quantity, IContext context)
-           => BestSpecialOfferProduct(p, (short) quantity, context).SpecialOffer ?? SpecialOffer_MenuFunctions.NoDiscount(context);
+            this Product p, [ValueRange(1, 999)] int quantity, IContext context)
+           => BestSpecialOfferProduct(p, (short)quantity, context).SpecialOffer ?? SpecialOffer_MenuFunctions.NoDiscount(context);
 
 
-        public static string DisableBestSpecialOffer(this Product p,  IContext context)
+        public static string DisableBestSpecialOffer(this Product p, IContext context)
          => p.IsDiscontinued(context) ? "Product is discontinued" : null;
 
         internal static SpecialOfferProduct BestSpecialOfferProduct(
-            this Product p, short quantity, IContext context) => 
+            this Product p, short quantity, IContext context) =>
             context.Instances<SpecialOfferProduct>().Where(obj => obj.Product.ProductID == p.ProductID &&
                               obj.SpecialOffer.StartDate <= DateTime.Now &&
                               obj.SpecialOffer.EndDate >= new DateTime(2004, 6, 1) &&
@@ -36,17 +38,17 @@ namespace AW.Functions {
 
         private static bool IsDiscontinued(this Product p, IContext context) =>
             p.DiscontinuedDate != null ? p.DiscontinuedDate.Value < context.Now() : false;
-        
+
         #endregion
 
         #region Associate with Special Offer
         public static (SpecialOfferProduct, IContext) AssociateWithSpecialOffer(
-            this Product product, SpecialOffer offer,IContext context)  => 
+            this Product product, SpecialOffer offer, IContext context) =>
             SpecialOffer_Functions.AssociateWithProduct(offer, product, context);
 
         [PageSize(20)]
         public static IQueryable<SpecialOffer> AutoComplete1AssociateWithSpecialOffer(
-            [MinLength(2)] string name, IContext context) => 
+            [MinLength(2)] string name, IContext context) =>
             context.Instances<SpecialOffer>().Where(specialOffer => specialOffer.Description.ToUpper().StartsWith(name.ToUpper()));
         #endregion
 
@@ -59,46 +61,53 @@ namespace AW.Functions {
 
         #endregion
 
+        #region Edits
+        [Edit]
         public static (Product, IContext) EditProductLine(this Product p,
             string productLine, IContext context) =>
             DisplayAndSave(p with { ProductLine = productLine }, context);
 
-        public static string[] ChoicesProductLine(this Product p)
+        public static string[] Choices1EditProductLine(this Product p)
         => new[] { "R ", "M ", "T ", "S " };  // nchar(2) in database so pad right with space
 
+        [Edit]
+        public static (Product, IContext) EditClass(this Product p,
+          string @class, IContext context) =>
+          DisplayAndSave(p with { Class = @class }, context);
 
-        #region Class
-        public static string[] ChoicesClass(this Product p)
-        => new[] { "H ", "M ", "L " }; // nchar(2) in database so pad right with space
-        #endregion
+        public static string[] Choices1EditClass(this Product p) =>
+            new[] { "H ", "M ", "L " }; // nchar(2) in database so pad right with space
 
-        #region Style
-        public static string[] ChoicesStyle(this Product p)
-        => new[] { "U ", "M ", "W " }; // nchar(2) in database so pad right with space
-        #endregion
+        [Edit]
+        public static (Product, IContext) EditStyle(this Product p,
+            string style, IContext context) =>
+                DisplayAndSave(p with { Style = style }, context);
 
-        #region Product Model
-        public static IQueryable<ProductModel> AutoCompleteProductModel(Product p, string match, IQueryable<ProductModel> models)
+        public static string[] Choices1EditStyle(this Product p) =>
+            new[] { "U ", "M ", "W " }; // nchar(2) in database so pad right with space
+
+        [Edit]
+        public static (Product, IContext) EditProductModel(this Product p,
+            ProductModel productModel, IContext context) =>
+                    DisplayAndSave(p with { ProductModel = productModel }, context);
+
+
+        public static IQueryable<ProductModel> AutoComplete1EditProductModel(this Product p,
+            [MinLength(3)] string match, IQueryable<ProductModel> models)
         {
             return models.Where(pm => pm.Name.ToUpper().Contains(match.ToUpper()));
         }
+
+        [Edit]
+        public static (Product, IContext) EditCategories(this Product p,
+      ProductCategory category, ProductSubcategory subCategory, IContext context) =>
+              DisplayAndSave(p with { ProductCategory = category, ProductSubcategory = subCategory }, context);
+
+        public static ProductSubcategory[] Choices2EditCategories(this Product p,
+            ProductCategory productCategory, IContext context) =>
+            productCategory is null ? new ProductSubcategory[] { }
+            : context.Instances<ProductSubcategory>().Where(psc => psc.ProductCategory.ProductCategoryID == productCategory.ProductCategoryID).ToArray();
+
         #endregion
-
-        #region Categories
-        public static IList<ProductSubcategory> ChoicesCategories(
-            Product p,
-            ProductCategory productCategory,
-            IQueryable<ProductSubcategory> subCats)
-        {
-            if (productCategory != null)
-            {
-                return subCats.Where(psc => psc.ProductCategory.ProductCategoryID == productCategory.ProductCategoryID).ToList();
-            }
-            return new ProductSubcategory[] { }.ToList();
-        }
-        #endregion
- 
-
-
     }
 }
