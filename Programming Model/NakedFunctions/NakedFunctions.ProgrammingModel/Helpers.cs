353f2a7e66b26b01b8db29a398482347e6df515c
Richard Pawson <rpawson@nakedobjects.net>
2021-01-20 17:51:32 +0000

    Add Helpers to NF.PM

---
+++ Programming Model/NakedFunctions/NakedFunctions.ProgrammingModel/Helpers.cs
@@ -0,0 +1,7 @@
+﻿namespace NakedFunctions
+{
+    public static class Helpers
+    {
+        public static (T, IContext) SaveAndDisplay<T>(T obj, IContext context) => (obj, context.WithPendingSave(obj));
+    }
+}
