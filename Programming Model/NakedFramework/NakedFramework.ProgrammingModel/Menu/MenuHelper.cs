226de150dbd61ce13fde94f4ae2853b43b3a2e09
Richard Pawson <rpawson@nakedobjects.net>
2021-01-24 12:24:21 +0000

    Move menu helper to Naked.Framework

---
+++ Programming Model/NakedFramework/NakedFramework.ProgrammingModel/Menu/MenuHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace NakedFramework
+{
+    public static class MenuHelper
+    {
+        /// <summary>
+        /// Given a list of types, returns a function, compatible with the requirements of the 
+        /// NakedCoreOptions.MainMenus property used in systems configuration that will generate
+        /// a main menu from each type, including all the available actions in each case. 
+        /// </summary>
+        /// <param name="typesDefiningMainMenuActions"></param>
+        /// <returns></returns>
+        public static Func<IMenuFactory, IMenu[]> GenerateMenus(Type[] typesDefiningMainMenuActions) =>
+                mf => typesDefiningMainMenuActions.Select(t => mf.NewMenu(t, true)).ToArray();
+    }
+}
