4d4c620414edf2fc75cacd3320dc8db9f0fb5072
Richard Pawson <rpawson@nakedobjects.net>
2021-01-27 12:28:18 +0000

    AW model

--- Test/AdventureWorksFunctionalModel/Human Resources/Employee.cs
+++ Test/AdventureWorksFunctionalModel/Human Resources/Employee.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Collections.Generic;
 using NakedFunctions;
-using static AW.Utilities;
 
 namespace AW.Types
 {
@@ -16,9 +15,6 @@ namespace AW.Types
 
     public record Employee : IEmployee, IHasRowGuid, IHasModifiedDate
     {
-        [Hidden]
-        public virtual int PersonDetailsID { get; init; }
-
         [MemberOrder(1)]
         public virtual Person PersonDetails { get; init; }
 
--- Test/AdventureWorksFunctionalModel/Person/Person_MenuFunctions.cs
+++ Test/AdventureWorksFunctionalModel/Person/Person_MenuFunctions.cs
@@ -11,45 +11,34 @@ using NakedFunctions;
 using AW.Types;
 
 using static AW.Helpers;
-using System;
 using System.Collections.Immutable;
 
-namespace AW.Functions {
+namespace AW.Functions
+{
     [Named("Persons")]
-    public static class Person_MenuFunctions {
-
-        [TableView(true, nameof(Person.AdditionalContactInfo))]
-        public static IQueryable<Person> FindContactByName(
+    public static class Person_MenuFunctions
+    {
+        public static IQueryable<Person> FindPersonsByName(
             [Optionally] string firstName, string lastName, IContext context) =>
                 context.Instances<Person>().Where(p => (firstName == null || p.FirstName.ToUpper().StartsWith(firstName.ToUpper())) &&
                       p.LastName.ToUpper().StartsWith(lastName.ToUpper())).OrderBy(p => p.LastName).ThenBy(p => p.FirstName);
 
-        public static Person RandomContact(IContext context) {
+        public static Person RandomPerson(IContext context)
+        {
             return Random<Person>(context);
-        }
-
-        [TableView(true, nameof(Person.AdditionalContactInfo))]
-        public static IList<Person> RandomContacts(IContext context) {
-            var instances = context.Instances<Person>().OrderBy(n => "");
-            IRandom random1 = context.GetService<IRandomSeedGenerator>().Random;
-            IRandom random2 = random1.Next();
-            Person p1 = instances.Skip(random1.ValueInRange(instances.Count())).FirstOrDefault();
-            Person p2 = instances.Skip(random2.ValueInRange(instances.Count())).FirstOrDefault();
-            return new[] { p1, p2 };
         }
 
         //To demonstrate use of recursion to create & use multiple random numbers
         public static IList<Person> RandomPersons(int numberRequired, IContext context) =>
             RandomPersons(numberRequired, context.Instances<Person>().OrderBy(p => ""), context.RandomSeed()).ToList();
 
-        //Test if an immutablelist can be returned
         internal static ImmutableList<Person> RandomPersons(
             int num, IOrderedQueryable<Person> source, IRandom random) =>
-             num < 1 ? ImmutableList<Person>.Empty:
+             num < 1 ? ImmutableList<Person>.Empty :
              ImmutableList.Create(RandomPerson(source, random)).AddRange(RandomPersons(num - 1, source, random.Next()));
 
-    internal static Person RandomPerson(IOrderedQueryable<Person> source, IRandom random) =>
-       source.Skip(random.ValueInRange(source.Count())).First();
+        internal static Person RandomPerson(IOrderedQueryable<Person> source, IRandom random) =>
+           source.Skip(random.ValueInRange(source.Count())).First();
 
     }
 }
