1b629f16bfc3472fc63b8d2185c72683f086b2ea
Richard Pawson <rpawson@nakedobjects.net>
2021-02-05 12:57:07 +0000

    Update Vendor_Functions.cs

--- Test/AdventureWorksFunctionalModel/Purchasing/Vendor_Functions.cs
+++ Test/AdventureWorksFunctionalModel/Purchasing/Vendor_Functions.cs
@@ -10,16 +10,17 @@ namespace AW.Functions
 {
     public static class Vendor_Functions
     {
+        [MemberOrder(1)]
         public static List<Product> ShowAllProducts(this Vendor vendor) =>
             vendor.Products.Select(vp => vp.Product).ToList();
 
         //Not implemented.  Action is to test disable function only.
-        [DescribedAs("Get report from credit agency")]
+        [MemberOrder(2), DescribedAs("Get report from credit agency")]
         public static Vendor CheckCredit(this Vendor v, IContext context) => v;
 
         public static string DisableCheckCredit(this Vendor v) => "Not yet implemented";
 
-
+        [MemberOrder("Purchase Orders", 2)]
         public static IQueryable<PurchaseOrderHeader> OpenPurchaseOrders(this Vendor vendor, IContext context) =>
             PurchaseOrder_MenuFunctions.OpenPurchaseOrdersForVendor(vendor, context);
 
@@ -27,7 +28,7 @@ namespace AW.Functions
             PurchaseOrder_MenuFunctions.AutoComplete0OpenPurchaseOrdersForVendor(name, context);
 
 
-        [MemberOrder("Purchase Orders",4)]
+        [MemberOrder("Purchase Orders",3)]
         [TableView(true, "OrderDate", "Status", "TotalDue")]
         public static IQueryable<PurchaseOrderHeader> ListPurchaseOrders(
             this Vendor vendor, DateTime? fromDate, DateTime? toDate, IContext context) =>
@@ -44,7 +45,7 @@ namespace AW.Functions
            this Vendor vendor, DateTime? fromDate, DateTime? toDate) =>
                 PurchaseOrder_MenuFunctions.ValidateListPurchaseOrders(vendor, fromDate, toDate);
 
-        [MemberOrder("Purchase Orders", 5)]
+        [MemberOrder("Purchase Orders", 1)]
         public static (PurchaseOrderHeader, IContext) CreateNewPurchaseOrder(this Vendor vendor,
            ShipMethod shipMethod, IContext context) =>
                 PurchaseOrder_MenuFunctions.CreateNewPurchaseOrder(vendor, shipMethod, context);
