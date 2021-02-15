618c3e84c643f05d57a9b6ed43f321d919721d1c
Richard Pawson <rpawson@nakedobjects.net>
2021-02-15 09:29:25 +0000

    Update SalesOrderHeader_Functions.cs

--- Test/AdventureWorksFunctionalModel/Sales/SalesOrderHeader_Functions.cs
+++ Test/AdventureWorksFunctionalModel/Sales/SalesOrderHeader_Functions.cs
@@ -42,7 +42,7 @@ namespace AW.Functions {
             return c =>
             {
                 var subTotal = soh.Details.Any()? soh.Details.Sum(d => d.LineTotal) : 0.0m;  //TODO: Works if just return 0.0m !
-                var tax = subTotal * soh.GetTaxRate(c) / 100;
+                var tax = subTotal;// * soh.GetTaxRate(c) / 100;
                 var total = subTotal + tax;
                 return c.WithUpdated(soh, soh with
                 {
