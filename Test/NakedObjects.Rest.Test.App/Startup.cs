5e4f1d59d3fb0c558d4463206a29e0e79fa7af52
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-02-25 13:40:00 +0000

    fix deploy

--- Test/NakedObjects.Rest.Test.App/Startup.cs
+++ Test/NakedObjects.Rest.Test.App/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NakedObjects.Architecture.Component;
+using NakedFramework.Architecture.Component;
 using NakedObjects.DependencyInjection.Extensions;
 using NakedObjects.Reflector.Extensions;
 using NakedObjects.Rest.Extensions;
