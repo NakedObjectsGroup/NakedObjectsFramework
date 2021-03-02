9558f5ddc07b097cc3c0cb369cb02668a7f38bba
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-03-02 08:42:55 +0000

    Tidy efcore persistor

--- NakedFramework/NakedFramework.Persistor.EFCore/Extensions/EFCorePersistorOptions.cs
+++ NakedFramework/NakedFramework.Persistor.EFCore/Extensions/EFCorePersistorOptions.cs
@@ -1,9 +1,9 @@
-﻿// // Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
-// // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
-// // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and limitations under the License.
+﻿// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
 
 using System;
 using Microsoft.EntityFrameworkCore;
@@ -11,14 +11,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace NakedFramework.Persistor.EFCore.Extensions {
     public class EFCorePersistorOptions {
-        //public MergeOption DefaultMergeOption { get; set; } = MergeOption.AppendOnly;
-        //public bool EnforceProxies { get; set; }  = true;
-        //public Func<bool> IsInitializedCheck { get; set; } = () => true;
-        //public int MaximumCommitCycles { get; set; } = 10;
-        //public Func<Type[]> NotPersistedTypes { get; set; }  = () => new Type[] {};
-        //public bool RollBackOnError { get; set; } = false;
+        public int MaximumCommitCycles { get; set; } = 10;
         public Func<IConfiguration, DbContext> ContextInstaller { get; set; }
-        //public Action<ObjectContext> CustomConfig { get; set; } = oc => { };
-        //public bool RequireExplicitAssociationOfTypes { get; set; } = false;
     }
 }
