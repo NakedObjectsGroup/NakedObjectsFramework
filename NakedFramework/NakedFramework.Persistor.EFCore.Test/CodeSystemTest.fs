f198d38c5eb9226d7025fab9f1a9e4dbac62e0dd
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-03-29 11:08:51 +0100

    EF Core test work

---
+++ NakedFramework/NakedFramework.Persistor.EFCore.Test/CodeSystemTest.fs
@@ -0,0 +1,25 @@
+﻿module NakedFramework.CodeSystemTest
+
+open NakedObjects.CodeSystemTest
+open NUnit.Framework
+open System
+open NakedFramework.DependencyInjection.Extensions
+open Microsoft.Extensions.Configuration
+open NakedFramework.Persistor.EFCore.Extensions
+open Microsoft.EntityFrameworkCore
+open TestData
+
+[<TestFixture>]
+type EFCoreCodeSystemTests() = 
+    inherit CodeSystemTests()
+
+    override x.AddNakedFunctions = Action<NakedCoreOptions> (fun (builder) -> ())
+
+    member x.ContextInstaller = Func<IConfiguration, DbContext> (fun (c) -> 
+                  let context = new EFCoreTestDataContext(NakedObjects.TestTypes.csCS)
+                  context.Create()
+                  (context :> DbContext))
+        
+    member x.EFCorePersistorOptions = Action<EFCorePersistorOptions> (fun  (options) -> options.ContextInstaller <- x.ContextInstaller)
+
+
