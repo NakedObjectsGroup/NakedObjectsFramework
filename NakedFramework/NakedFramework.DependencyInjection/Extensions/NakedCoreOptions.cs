96b29eaf411068258c157a096bdd376b4889a67e
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-04-13 17:38:49 +0100

    Rename Entity persistor

--- NakedFramework/NakedFramework.DependencyInjection/Extensions/NakedCoreOptions.cs
+++ NakedFramework/NakedFramework.DependencyInjection/Extensions/NakedCoreOptions.cs
@@ -13,7 +13,7 @@ using NakedFramework.Metamodel.Authorization;
 using NakedFramework.Metamodel.Profile;
 
 [assembly: InternalsVisibleTo("NakedFunctions.Reflector")]
-[assembly: InternalsVisibleTo("NakedFramework.Persistor.Entity")]
+[assembly: InternalsVisibleTo("NakedFramework.Persistor.EF6")]
 [assembly: InternalsVisibleTo("NakedFramework.Persistor.EFCore")]
 
 namespace NakedFramework.DependencyInjection.Extensions {
