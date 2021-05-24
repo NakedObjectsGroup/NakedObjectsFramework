5b6530b06d856427a625c122b5187896f26e3f27
Richard Pawson <rpawson@nakedobjects.net>
2021-05-24 16:05:21 +0100

    Update (NO) template model

--- Template/Naked Objects Server/Template.Model/ModelConfig.cs
+++ Template/Naked Objects Server/Template.Model/ModelConfig.cs
@@ -3,13 +3,7 @@ using System;
 
 namespace Template.Model
 {
-    //A helper class to provide model configuration for use by Startup.cs in the Server project.
-    //The implementation here relies on the conventions that:
-    //- All domain classes are defined in namespace "Template.Model.Types"
-    //- All domain functions are defined on static types in namespace "Template.Model.Functions"
-    //- All main menu function are defined on static types that have 'MenuFunctions' in their name
-    //This ModelConfig may be re-written to change the conventions, or to remove conventions altogether, and
-    //specify the lists of types, functions, and menus explicitly.
+
     public static class ModelConfig
     {
         public static Type[] DomainModelTypes() => new[] { typeof(Student) };
