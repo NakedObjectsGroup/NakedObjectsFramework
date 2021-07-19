4588a4e20aecabd4dfe851846ef95da3c1ce99b0
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-07-19 11:47:40 +0100

    Fix test

--- NakedFramework/NakedFramework.Rest.Test/Version8.fs
+++ NakedFramework/NakedFramework.Rest.Test/Version8.fs
@@ -29,7 +29,7 @@ let internal links =
 let internal expected = 
     [ TProperty(JsonPropertyNames.Links, links)
       TProperty(JsonPropertyNames.SpecVersion, TObjectVal("1.2"))
-      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("Naked Objects 12.0.0-beta04"))
+      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("Naked Objects 12.0.0-rc01"))
       TProperty(JsonPropertyNames.OptionalCapabilities, capabilities)
       TProperty(JsonPropertyNames.Extensions, TObjectJson([])) ]
 
