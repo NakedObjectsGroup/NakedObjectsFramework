06b8d322cff60eeb94893aef03fccd1cd7f52640
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-11-01 14:53:53 +0000

    Reducing boilerplate and adding dates

---
+++ Legacy/Legacy.NakedObjects/Application/System/SystemClock.cs
@@ -0,0 +1,40 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: org.nakedobjects.application.system.SystemClock
+// Assembly: nakedobjects.net, Version=2.1.5386.20054, Culture=neutral, PublicKeyToken=null
+// MVID: 1DE237A2-7EE8-405D-B628-89F8099DF058
+// Assembly location: C:\Users\scasc\Documents\sdm\SdmApp-Dlls\bin\nakedobjects.net.dll
+
+using System;
+using Legacy.NakedObjects.Application.ValueHolder;
+
+namespace Legacy.NakedObjects.Application.System
+{
+  //[JavaInterfaces("1;org/nakedobjects/application/system/Clock;")]
+  public class SystemClock : Clock
+  {
+    public SystemClock()
+    {
+      Date.setClock((Clock) this);
+      //Time.setClock((Clock) this);
+      //DateTime.setClock((Clock) this);
+      //TimeStamp.setClock((Clock) this);
+    }
+
+    public virtual long getTime() {
+            //  return global::System.currentTimeMillis();
+            throw new NotImplementedException();
+        }
+
+    //[JavaFlags(4227077)]
+    //[JavaThrownExceptions("1;java/lang/CloneNotSupportedException;")]
+    //public new virtual object MemberwiseClone()
+    //{
+    //  SystemClock systemClock = this;
+    //  ObjectImpl.clone((object) systemClock);
+    //  return ((object) systemClock).MemberwiseClone();
+    //}
+
+    ////[JavaFlags(4227073)]
+    //public override string ToString() => ObjectImpl.jloToString((object) this);
+  }
+}
