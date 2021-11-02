2fe9f0269e06f20af3ecb8832dabb62f6b70ef50
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-11-02 17:31:09 +0000

    Add minimal TextString

---
+++ Programming Model/Legacy/Legacy.Types/Legacy.Types/Value holders/TextString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace Legacy.Types {
+    public class TextString {
+        private readonly string text;
+
+        public TextString(string text) => this.text = text;
+
+        public TextString(string text, Action<string> callback) : this(text) => BackingField = callback;
+
+        private Action<string> BackingField { get; set; } = _ => { };
+
+        public override string ToString() => text;
+    }
+}
