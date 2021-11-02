911c0a870a95210100f3fcded52170d7308d7dd2
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-11-02 18:16:06 +0000

    Create WholeNumber.cs

---
+++ Programming Model/Legacy/Legacy.Types/Legacy.Types/Value holders/WholeNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Legacy.Types {
+    public class TextString {
+        private string text;
+
+        // necessary for when used as a parameter
+        public TextString(string text) => Text = text;
+
+        public TextString(string text, Action<string> callback) : this(text) => UpdateBackingField = callback;
+
+        public string Text {
+            get => text;
+            set {
+                text = value;
+                UpdateBackingField(text);
+            }
+        }
+
+        private Action<string> UpdateBackingField { get; } = _ => { };
+
+        public override string ToString() => Text;
+    }
+}
