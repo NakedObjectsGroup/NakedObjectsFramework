6153006217da25f8ddab12cc4d754aec80ddc892
Stefano Cascarini <stef.cascarini@googlemail.com>
2021-11-06 14:53:24 +0000

    Bump versions prep for .net 6

--- NakedFramework/NakedFramework.Rest.Test/Version8.fs
+++ NakedFramework/NakedFramework.Rest.Test/Version8.fs
@@ -29,7 +29,7 @@ let internal links =
 let internal expected = 
     [ TProperty(JsonPropertyNames.Links, links)
       TProperty(JsonPropertyNames.SpecVersion, TObjectVal("1.2"))
-      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("Naked Objects 12.0.0"))
+      TProperty(JsonPropertyNames.ImplVersion, TObjectVal("Naked Objects 12.1.0"))
       TProperty(JsonPropertyNames.OptionalCapabilities, capabilities)
       TProperty(JsonPropertyNames.Extensions, TObjectJson([])) ]
 
