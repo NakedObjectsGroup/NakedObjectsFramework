27e42400e624487199359a8e44081a54ef0e7a9a
Richard Pawson <rpawson@nakedobjects.net>
2021-11-09 09:44:58 +0000

    Special offer working

--- Test/AdventureWorksLegacyModel/Sales/SpecialOffer.cs
+++ Test/AdventureWorksLegacyModel/Sales/SpecialOffer.cs
@@ -90,21 +90,21 @@ namespace AdventureWorksModel
         #region EditMinQty (Legacy Action)
         public void ActionEditMinQty(WholeNumber minQty) => MinQty.Number = minQty.Number;
 
-        public void AboutActionEditMinQty(ActionAbout a, WholeNumber minQty)
-        {
-            switch (a.TypeCode)
-            {
-                case AboutTypeCodes.Parameters:
-                    a.ParamDefaultValues[0] = MinQty;
-                    break;
-                case AboutTypeCodes.Usable:
-                    a.UnusableReason = ValidateEditMinQty(minQty.Number);
-                    a.Usable = string.IsNullOrEmpty(a.UnusableReason);              
-                    break;
-                default: 
-                    break;
-            }
-        }
+        //public void AboutActionEditMinQty(ActionAbout a, WholeNumber minQty)
+        //{
+        //    switch (a.TypeCode)
+        //    {
+        //        case AboutTypeCodes.Parameters:
+        //            a.ParamDefaultValues[0] = MinQty;
+        //            break;
+        //        case AboutTypeCodes.Usable:
+        //            a.UnusableReason = ValidateEditMinQty(minQty.Number);
+        //            a.Usable = string.IsNullOrEmpty(a.UnusableReason);              
+        //            break;
+        //        default: 
+        //            break;
+        //    }
+        //}
 
         private string ValidateEditMinQty(int minQty) => minQty > 0 ? null : "Min Qty must be > 0";
         #endregion
