66734cfa97fb34ffe3466412450618ce8d6d6032
Richard Pawson <rpawson@nakedobjects.net>
2021-11-09 18:05:44 +0000

    Additions/changes hanges to Legacy.PM

--- Programming Model/NakedLegacy/NakedLegacy.Types/Control (abouts)/FieldAboutImpl.cs
+++ Programming Model/NakedLegacy/NakedLegacy.Types/Control (abouts)/FieldAboutImpl.cs
@@ -6,5 +6,17 @@
 
         public bool Usable { get; set; } = true;
         public bool Visible { get; set; } = true;
+
+        public bool IsPersistent {get; set;}
+
+        public bool Required { get; set; }
+        public object DefaultValue { get; set; }
+        public object[] Options { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UnusableReason { get; set; }
+
+        public bool IsValid { get; set; }
+        public string InvalidReason { get; set; }
     }
 }
