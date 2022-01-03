0ebf2b7db7a6387dfda6beb359ec70ef64cc50e5
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-01-03 15:40:35 +0000

    Update build.bat for legacy

--- build.bat
+++ build.bat
@@ -1,5 +1,12 @@
 SET nugetPackagePath=C:\NakedObjectsPackages
 
+DEL ".\Programming Model\NakedFramework\NakedFramework.ProgrammingModel.Package\bin\Debug\*.nupkg"
+DEL ".\Programming Model\NakedObjects\NakedObjects.ProgrammingModel.Package\bin\Debug\*.nupkg"
+DEL ".\Programming Model\NakedFunctions\NakedFunctions.ProgrammingModel.Package\bin\Debug\*.nupkg"
+DEL ".\Programming Model\NakedLegacy\NakedLegacy.ProgrammingModel.Package\bin\Debug\*.nupkg"
+DEL ".\NakedObjects\NakedObjects.Server.Package\bin\Debug\*.nupkg"
+DEL ".\NakedFunctions\NakedFunctions.Server.Package\bin\Debug\*.nupkg"
+
 dotnet build NakedFramework.ProgrammingModel.sln -c Debug
 
 dotnet pack "Programming Model\NakedFramework\NakedFramework.ProgrammingModel.Package\NakedFramework.ProgrammingModel.Package.csproj"  --include-symbols --include-source
@@ -8,12 +15,15 @@ XCOPY ".\Programming Model\NakedFramework\NakedFramework.ProgrammingModel.Packag
 
 dotnet build NakedObjects.ProgrammingModel.sln -c Debug
 dotnet build NakedFunctions.ProgrammingModel.sln -c Debug
+dotnet build NakedLegacy.ProgrammingModel.sln -c Debug
 
 dotnet pack "Programming Model\NakedObjects\NakedObjects.ProgrammingModel.Package\NakedObjects.ProgrammingModel.Package.csproj"  --include-symbols --include-source
 dotnet pack "Programming Model\NakedFunctions\NakedFunctions.ProgrammingModel.Package\NakedFunctions.ProgrammingModel.Package.csproj"  --include-symbols --include-source
+dotnet pack "Programming Model\NakedLegacy\NakedLegacy.ProgrammingModel.Package\NakedLegacy.ProgrammingModel.Package.csproj"  --include-symbols --include-source
 
 XCOPY ".\Programming Model\NakedObjects\NakedObjects.ProgrammingModel.Package\bin\Debug\*.nupkg"  %nugetPackagePath% /y
 XCOPY ".\Programming Model\NakedFunctions\NakedFunctions.ProgrammingModel.Package\bin\Debug\*.nupkg"  %nugetPackagePath% /y
+XCOPY ".\Programming Model\Nakedlegacy\NakedLegacy.ProgrammingModel.Package\bin\Debug\*.nupkg"  %nugetPackagePath% /y
 
 dotnet build NakedObjects.Server.sln -c Debug
 dotnet build NakedFunctions.Server.sln -c Debug
