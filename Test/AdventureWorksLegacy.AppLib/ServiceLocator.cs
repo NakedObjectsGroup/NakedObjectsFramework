fe848bb7e29b927e031cbad61551d79a5401bb35
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-01-11 15:23:08 +0000

    #352 add service locator

---
+++ Test/AdventureWorksLegacy.AppLib/ServiceLocator.cs
@@ -0,0 +1,11 @@
+﻿using NakedLegacy.Types.Container;
+
+namespace AdventureWorksLegacy.AppLib;
+
+public class ServiceLocator {
+    private readonly IContainer container;
+
+    public ServiceLocator(IContainer container) => this.container = container;
+
+    public object Repository(Type ofType) => container.Repository(ofType);
+}
