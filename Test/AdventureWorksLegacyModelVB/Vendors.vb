7efaf52e447f3c451299b86e03f5b5a6554cfc45
Richard Pawson <rpawson@nakedobjects.net>
2022-01-11 12:12:37 +0000

    #349 testing

--- Test/AdventureWorksLegacyModelVB/Vendors.vb
+++ Test/AdventureWorksLegacyModelVB/Vendors.vb
@@ -2,14 +2,23 @@
 
     Public Class Vendors
 
+        Public Shared Function ActionOrder() As String
+            Return "FindVendorByName,FindVendorByAccountNumber,  Randomvendor"
+        End Function
 
         Public Shared Function ActionRandomVendor() As Vendor
             Return SimpleRepository.Random(Of Vendor)()
         End Function
 
-        Public Shared Function ActionRandomProductVendor() As ProductVendor
-            Return SimpleRepository.Random(Of ProductVendor)()
+        Public Shared Function ActionFindVendorByName() As ArrayList
+            Throw New NotImplementedException()
         End Function
+
+        Public Shared Function ActionFindVendorByAccountNumber() As ArrayList
+            Throw New NotImplementedException()
+        End Function
+
+
 
     End Class
 End Namespace
