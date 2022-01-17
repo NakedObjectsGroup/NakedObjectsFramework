1d322a9a5c4e554ddeca61d9e16f525e06a2410a
Richard Pawson <rpawson@nakedobjects.net>
2022-01-17 14:50:28 +0000

    Fix tests

--- Test/AdventureWorksLegacy.AppLib/menu/MenuAction.cs
+++ Test/AdventureWorksLegacy.AppLib/menu/MenuAction.cs
@@ -3,7 +3,7 @@ namespace AdventureWorksLegacy.AppLib;
 
 public class MenuAction : IMenuAction
 {
-    public MenuAction(string name) => Name = name.ToLower().StartsWith("action") ? name : "Action" + name;
+    public MenuAction(string name) => Name = name.ToLower().StartsWith("action")  ? name : "Action" + name;
 
     public string Name { get; init; }
 }
