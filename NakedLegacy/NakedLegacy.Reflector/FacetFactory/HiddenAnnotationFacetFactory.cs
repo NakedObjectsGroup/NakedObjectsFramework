aaf1fe34c0f584a7a8034b6b2b2ace2614f3c29a
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-01-26 18:29:30 +0000

    #379 implement INamed

---
+++ NakedLegacy/NakedLegacy.Reflector/FacetFactory/HiddenAnnotationFacetFactory.cs
@@ -0,0 +1,51 @@
+// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+using System;
+using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+using NakedFramework;
+using NakedFramework.Architecture.Component;
+using NakedFramework.Architecture.Facet;
+using NakedFramework.Architecture.FacetFactory;
+using NakedFramework.Architecture.Reflect;
+using NakedFramework.Architecture.Spec;
+using NakedFramework.Architecture.SpecImmutable;
+using NakedFramework.Metamodel.Facet;
+using NakedFramework.Metamodel.Utils;
+using NakedLegacy.Attribute;
+using NakedLegacy.Reflector.Helpers;
+
+namespace NakedLegacy.Reflector.FacetFactory;
+
+public sealed class HiddenAnnotationFacetFactory : LegacyFacetFactoryProcessor, IAnnotationBasedFacetFactory {
+    public HiddenAnnotationFacetFactory(IFacetFactoryOrder<HiddenAnnotationFacetFactory> order, ILoggerFactory loggerFactory)
+        : base(order.Order, loggerFactory, FeatureType.PropertiesCollectionsAndActions) { }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, Type type, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        Process(type, specification);
+        return metamodel;
+    }
+
+    private static void Process(object onObject, ISpecification specification) {
+        var attribute = onObject.GetCustomAttribute<IHiddenAttribute>();
+        FacetUtils.AddFacet(Create(attribute, specification));
+    }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, MethodInfo method, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        Process(method, specification);
+        return metamodel;
+    }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, PropertyInfo property, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        Process(property, specification);
+        return metamodel;
+    }
+
+    private static IHiddenFacet Create(IHiddenAttribute attribute, ISpecification holder) => attribute is null ? null : new HiddenFacet(WhenTo.Always, holder);
+}
---
+++ NakedLegacy/NakedLegacy.Reflector/FacetFactory/NamedAnnotationFacetFactory.cs
@@ -0,0 +1,68 @@
+// Copyright Naked Objects Group Ltd, 45 Station Road, Henley on Thames, UK, RG9 1AT
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+using System;
+using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+using NakedFramework.Architecture.Component;
+using NakedFramework.Architecture.Facet;
+using NakedFramework.Architecture.FacetFactory;
+using NakedFramework.Architecture.Reflect;
+using NakedFramework.Architecture.Spec;
+using NakedFramework.Architecture.SpecImmutable;
+using NakedFramework.Metamodel.Facet;
+using NakedFramework.Metamodel.Utils;
+using NakedLegacy.Attribute;
+using NakedLegacy.Reflector.Helpers;
+
+namespace NakedLegacy.Reflector.FacetFactory;
+
+public sealed class NamedAnnotationFacetFactory : LegacyFacetFactoryProcessor, IAnnotationBasedFacetFactory {
+    private readonly ILogger<NamedAnnotationFacetFactory> logger;
+
+    public NamedAnnotationFacetFactory(IFacetFactoryOrder<NamedAnnotationFacetFactory> order, ILoggerFactory loggerFactory)
+        : base(order.Order, loggerFactory, FeatureType.Everything) =>
+        logger = loggerFactory.CreateLogger<NamedAnnotationFacetFactory>();
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, Type type, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        var attribute = type.GetCustomAttribute<INamedAttribute>();
+        FacetUtils.AddFacet(Create(attribute, specification));
+        return metamodel;
+    }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, MethodInfo method, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        var attribute = method.GetCustomAttribute<INamedAttribute>();
+        FacetUtils.AddFacet(CreateForMember(attribute, specification));
+        return metamodel;
+    }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> Process(IReflector reflector, PropertyInfo property, IMethodRemover methodRemover, ISpecificationBuilder specification, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        var attribute = property.GetCustomAttribute<INamedAttribute>();
+        FacetUtils.AddFacet(CreateForMember(attribute, specification));
+        return metamodel;
+    }
+
+    public override IImmutableDictionary<string, ITypeSpecBuilder> ProcessParams(IReflector reflector, MethodInfo method, int paramNum, ISpecificationBuilder holder, IImmutableDictionary<string, ITypeSpecBuilder> metamodel) {
+        var parameter = method.GetParameters()[paramNum];
+        var attribute = parameter.GetCustomAttribute<INamedAttribute>();
+        FacetUtils.AddFacet(CreateForMember(attribute, holder));
+        return metamodel;
+    }
+
+    private INamedFacet Create(INamedAttribute attribute, ISpecification holder) =>
+        attribute switch {
+            { } namedAttribute => new NamedFacetAnnotation(namedAttribute.Name, holder),
+            _ => null
+        };
+
+    private IMemberNamedFacet CreateForMember(INamedAttribute attribute, ISpecification holder) =>
+        attribute switch {
+            { } namedAttribute => new MemberNamedFacetAnnotation(namedAttribute.Name, holder),
+            _ => null
+        };
+}
