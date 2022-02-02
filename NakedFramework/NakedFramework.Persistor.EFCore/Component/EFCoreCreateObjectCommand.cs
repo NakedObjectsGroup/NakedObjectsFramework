12c5ec5d4f057539532b3793de691f2b45217b43
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-02-02 14:13:58 +0000

    #376 add to context after values copied to proxy

--- NakedFramework/NakedFramework.Persistor.EFCore/Component/EFCoreCreateObjectCommand.cs
+++ NakedFramework/NakedFramework.Persistor.EFCore/Component/EFCoreCreateObjectCommand.cs
@@ -76,7 +76,6 @@ public class EFCoreCreateObjectCommand : ICreateObjectCommand {
         var persisting = entry.State == EntityState.Detached;
         if (persisting) {
             SetKeyAsNecessary(originalObject, objectToAdd);
-            dbContext.Add(objectToAdd);
         }
         else {
             objectToAdd = originalObject;
@@ -96,6 +95,7 @@ public class EFCoreCreateObjectCommand : ICreateObjectCommand {
             // with already known object in identity map when replacing the poco
             parent.RemoveAdapter(proxyAdapter);
             parent.ReplacePoco(adapterForOriginalObjectAdapter, objectToAdd);
+            dbContext.Add(objectToAdd);
         }
         else {
             SetKeyAsNecessary(originalObject, objectToAdd);
