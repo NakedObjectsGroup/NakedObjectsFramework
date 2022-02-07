6755613272f5a4a72c883167905543d450a9b7ac
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-02-07 15:21:29 +0000

    #399 fix usings

--- Programming Model/NOF2/NOF2.Types/Collection/InternalCollection.cs
+++ Programming Model/NOF2/NOF2.Types/Collection/InternalCollection.cs
@@ -5,6 +5,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace NOF2.Collection {
     public interface InternalCollection : IList { }
 
--- Programming Model/NOF2/NOF2.Types/Container/IContainer.cs
+++ Programming Model/NOF2/NOF2.Types/Container/IContainer.cs
@@ -5,6 +5,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace NOF2.Container {
     public interface IContainer {
         void AddMessageToBroker(string message);
