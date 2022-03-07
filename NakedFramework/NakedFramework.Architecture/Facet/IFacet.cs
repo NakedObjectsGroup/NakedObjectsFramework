c43b9e78cf3294517641a8e477a5ca8be873d96f
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-03-07 11:43:07 +0000

    #410 start facet restructure

--- NakedFramework/NakedFramework.Architecture/Facet/IFacet.cs
+++ NakedFramework/NakedFramework.Architecture/Facet/IFacet.cs
@@ -10,7 +10,7 @@ using NakedFramework.Architecture.Spec;
 
 namespace NakedFramework.Architecture.Facet;
 
-public interface IFacet {
+public interface IFacet : IUndecoratedFacet {
     /// <summary>
     ///     The <see cref="ISpecification" /> of this facet.
     /// </summary>
@@ -18,39 +18,4 @@ public interface IFacet {
     ///     Used by Facet decorators.
     /// </para>
     ISpecification Specification { get; }
-
-    /// <summary>
-    ///     Whether this facet implementation is a no-op
-    /// </summary>
-    bool IsNoOp { get; }
-
-    /// <summary>
-    ///     Determines the type of this facet to be stored under.
-    /// </summary>
-    /// <para>
-    ///     The framework looks for <see cref="IFacet" />s of certain well-known facet types.
-    ///     Each facet implementation must specify which type of facet it corresponds to.
-    ///     This therefore allows the (rules of the) programming model to be varied
-    ///     without impacting the rest of the framework.
-    /// </para>
-    /// <para>
-    ///     For example, the <see cref="IActionInvocationFacet" /> specifies the facet to
-    ///     invoke an action.  The typical implementation of this wraps a <c>public</c>
-    ///     method.  However, a different facet factory could be installed that creates
-    ///     facet also of type <see cref="IActionInvocationFacet" /> but that have some other
-    ///     rule, such as requiring an <i>action</i> prefix, or that decorate the
-    ///     interaction by logging it, for example.
-    /// </para>
-    Type FacetType { get; }
-
-    /// <summary>
-    ///     Whether this facet implementation should replace existing(none-noop) implementations.
-    ///     Other than CanNeverBeReplaced
-    /// </summary>
-    bool CanAlwaysReplace { get; }
-
-    /// <summary>
-    ///     Whether this facet implementation can be replaced.
-    /// </summary>
-    bool CanNeverBeReplaced { get; }
 }
