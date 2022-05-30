1191a77563d6ad6c19771a8fb9e81a61a7cc8029
Richard Pawson <rpawson@nakedobjects.net>
2022-05-30 18:19:15 +0100

    TestModelGenerator - added Attributes

--- Test/TestModelGenerator/Program.cs
+++ Test/TestModelGenerator/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.Write("FileName to write to (incl. extension): ");
 var fileName = Console.ReadLine();
+Console.Write("Namespace prefix: ");
+var prefix = Console.ReadLine();
 Console.Write("How many types? ");
 int n = Convert.ToInt32(Console.ReadLine());
-var g = new Generator(n);
-
+var g = new Generator(prefix, n);
 
 using (StreamWriter writer = new StreamWriter(@"..\..\..\"+fileName))
 {
+    writer.WriteLine("using NakedObjects;");
     for (int i = 0; i < n; i++)
     {
         writer.WriteLine(g.GenerateClass(i));
