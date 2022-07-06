052daaf690796dbe701d0c3e3ddf600bb25ddf95
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-07-06 11:00:42 +0100

    tidy some config

--- Template/SPA Client/Template.Client/ClientApp/proxy.conf.js
+++ Template/SPA Client/Template.Client/ClientApp/proxy.conf.js
@@ -6,8 +6,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
-   ],
+    ],
     target: target,
     secure: false,
     headers: {
