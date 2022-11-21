f3a547816e22e10a53b18694378d4fe359320913
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-11-21 18:11:27 +0000

    more work

--- API/ROSI/API.cs
+++ API/ROSI/API.cs
@@ -1,44 +1,10 @@
-﻿using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Text.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 
 namespace ROSI;
 
 public static class API {
-    private static readonly HttpClient Client = new();
-
-    private static HttpRequestMessage CreateMessage(HttpMethod method, string path, HttpContent? content = null) {
-        var request = new HttpRequestMessage(method, path);
-
-        if (content is not null) {
-            request.Content = content;
-        }
-
-        return request;
-    }
-
-    private static string ReadAsString(HttpResponseMessage response) {
-        using var sr = new StreamReader(response.Content.ReadAsStream());
-        var json = sr.ReadToEnd();
-        return json ?? "";
-    }
-
-    private static string Execute(string? jsonContent, string url) {
-        using var content = JsonContent.Create("", new MediaTypeHeaderValue("application/json"));
-        var request = CreateMessage(HttpMethod.Get, url, content);
-
-        using var response = Client.SendAsync(request).Result;
-
-        if (response.IsSuccessStatusCode) {
-            return ReadAsString(response);
-        }
-
-        throw new HttpRequestException("request failed", null, response.StatusCode);
-    }
-
     public static JObject GetObject(Uri uri) {
-        var json = Execute(null, uri.ToString());
+        var json = Helpers.Execute(null, uri.ToString());
         return JObject.Parse(json);
     }
 
---
+++ API/ROSI/Helpers.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace ROSI; 
+
+public static class Helpers {
+    private static readonly HttpClient Client = new();
+
+    private static HttpRequestMessage CreateMessage(HttpMethod method, string path, HttpContent? content = null) {
+        var request = new HttpRequestMessage(method, path);
+
+        if (content is not null) {
+            request.Content = content;
+        }
+
+        return request;
+    }
+
+    private static string ReadAsString(HttpResponseMessage response) {
+        using var sr = new StreamReader(response.Content.ReadAsStream());
+        var json = sr.ReadToEnd();
+        return json ?? "";
+    }
+
+    public static string Execute(string? jsonContent, string url) {
+        using var content = JsonContent.Create("", new MediaTypeHeaderValue("application/json"));
+        var request = CreateMessage(HttpMethod.Get, url, content);
+
+        using var response = Client.SendAsync(request).Result;
+
+        if (response.IsSuccessStatusCode) {
+            return ReadAsString(response);
+        }
+
+        throw new HttpRequestException("request failed", null, response.StatusCode);
+    }
+}
