9b0cfd45de4f120522943cecaa0787bc1af91005
Stefano Cascarini <stef.cascarini@googlemail.com>
2022-12-21 14:35:04 +0000

    Update ActionApi.cs

--- API/ROSI/Apis/ActionApi.cs
+++ API/ROSI/Apis/ActionApi.cs
@@ -7,6 +7,12 @@ using Action = ROSI.Records.Action;
 namespace ROSI.Apis;
 
 public static class ActionApi {
+
+    public static async Task<Action> GetDetails(this Action actionRepresentation, InvokeOptions? options = null) {
+        var json = await HttpHelpers.GetDetails(actionRepresentation, options ?? new InvokeOptions());
+        return new Action(JObject.Parse(json));
+    }
+
     public static async Task<ActionResult> Invoke(this IAction actionRepresentation, InvokeOptions? options = null) {
         var json = await HttpHelpers.Execute(actionRepresentation, options ?? new InvokeOptions());
         return new ActionResult(JObject.Parse(json));
