8f04bbeeb6cefff04ce5a558732f685b340b0b1b
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-01-03 14:51:01 +0000

    Update ExtensionsApi.cs

--- API/ROSI/Apis/ExtensionsApi.cs
+++ API/ROSI/Apis/ExtensionsApi.cs
@@ -34,7 +34,7 @@ public static class ExtensionsApi {
     private static object? ExtensionObject(this JProperty prop) =>
         prop.Value switch {
             JValue jv => jv.Value,
-            JObject jo => jo.Children().Cast<JProperty>().ToDictionary(c => c.Name, c => ((JValue)c.Value).Value),
+            JObject jo => jo.Children().Cast<JProperty>().ToDictionary(c => c.Name, c => c.ExtensionObject()),
             JArray ja => ja.Children().Cast<JValue>().Select(t => t.Value).ToList(),
             _ => null
         };
