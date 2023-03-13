fa0db17cc13d9421ac66b9359dffca59649517f9
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-03-13 11:25:50 +0000

    #464 move client to angular 15 and bump packages

--- Client/nakedobjects-client/src/app/app-routing.module.ts
+++ Client/nakedobjects-client/src/app/app-routing.module.ts
@@ -143,7 +143,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
     providers: []
 })
