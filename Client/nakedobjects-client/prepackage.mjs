9773179536d953b2465f5c1c16180f57ffc75369
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-07-22 15:34:30 +0100

    #527 fix build

---
+++ Client/nakedobjects-client/prepackage.mjs
@@ -0,0 +1,45 @@
+﻿/* eslint-disable */
+
+import { replaceInFile } from 'replace-in-file';
+import { find } from 'find-in-files';
+
+// get current version
+
+const s = await find("version", ".", "package.json");
+
+try {
+    var versionLine = s["package.json"].line[0];
+    var versionSplit = versionLine.split('"');
+    var version = versionSplit[3];
+
+    // to update client version in code
+    var options2 = {
+        files: ["./gemini/src/version.ts"],
+        from: [/clientVersion.*/g],
+        to: "clientVersion = '" + version + "';"
+    };
+
+    await replaceInFile(options2);
+} catch (e) {
+    console.error('Error occurred updating version:', e);
+}
+
+// var version = findSync("version", ".", "package.json").then(s => {
+
+//     try {
+//         var versionLine = s["package.json"].line[0];
+//         var versionSplit = versionLine.split('"');
+//         var version = versionSplit[3];
+
+//         // to update client version in code
+//         var options2 = {
+//             files: ["./gemini/src/version.ts"],
+//             from: [/clientVersion.*/g],
+//             to: "clientVersion = '" + version + "';"
+//         };
+
+//         await replaceInFile(options2);
+//     } catch (e) {
+//         console.error('Error occurred updating version:', e);
+//     }
+// });
